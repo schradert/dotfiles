9dc628650f84bd30e583207b872e486b392d2716
Tristan Schrader <tristan@climaxfoods.com>
2022-09-11 10:53:56 -0700

    fix linting errors in install script

--- install.sh
+++ install.sh
@@ -6,37 +6,37 @@ today=$(date +"%Y%m%d")
 config_d="$HOME/.config/dotfiles"
 backups_d="$config_d/backups"
 backup_d="$backups_d/$today"
-mkdir -p $backup_d
+mkdir -p "$backup_d"
 log_f="$backup_d/run.log"
 
 sub() {
     local i o dir
     local "${@}"
 
-    out="${o:-`basename $i`}"
+    out="${o:-$(basename "$i")}"
     [[ -d $i ]] && {
         local dir="/"
-        mkdir -p "$HOME/$out"
-        mkdir -p "$backup_d/$out"
+            mkdir -p "$HOME/$out"
+            mkdir -p "$backup_d/$out"
+        }
+        mkdir -p "$(dirname "$backup_d/$out")"
+
+        rsync -iarvz --ignore-existing "$HOME/$out$dir" "$backup_d/$out" | grep -E "^>" | tee -a "$log_f"
+        rsync -iarvz --exclude "**/*.swp" "$work_d/$i$dir" "$HOME/$out" | grep -E "^>" | tee -a "$log_f"
     }
-    mkdir -p `dirname "$backup_d/$out"`
 
-    rsync -iarvz --ignore-existing "$HOME/$out$dir" "$backup_d/$out" | egrep "^>" | tee -a $log_f
-    rsync -iarvz --exclude "**/*.swp" "$work_d/$i$dir" "$HOME/$out" | egrep "^>" | tee -a $log_f
-}
-
-sub i=zsh/.zshrc
-sub i=zsh/.zprofile
-sub i=zsh/.p10k.zsh
-sub i=zsh/config.d o=.config/zsh/config.d
-sub i=gh o=.config/gh
-sub i=git/.gitconfig
-sub i=glab o=.config/glab-cli
-sub i=htop o=.config/htop
-sub i=vim/.vimrc
-sub i=ranger o=.config/ranger
-sub i=thefuck o=.config/thefuck
-sub i=wtf o=.config/wtf
-sub i=nix/.nix-channels
-sub i=nix/darwin-configuration.nix o=.nixpkgs/darwin-configuration.nix
+    sub i=zsh/.zshrc
+    sub i=zsh/.zprofile
+    sub i=zsh/.p10k.zsh
+    sub i=zsh/config.d o=.config/zsh/config.d
+    sub i=gh o=.config/gh
+    sub i=git/.gitconfig
+    sub i=glab o=.config/glab-cli
+    sub i=htop o=.config/htop
+    sub i=vim/.vimrc
+    sub i=ranger o=.config/ranger
+    sub i=thefuck o=.config/thefuck
+    sub i=wtf o=.config/wtf
+    sub i=nix/.nix-channels
+    sub i=nix/darwin-configuration.nix o=.nixpkgs/darwin-configuration.nix
 
