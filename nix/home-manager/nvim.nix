0fa539fe03120472fb3377ba39ee3884478e8475
Tristan Schrader <tristan@climaxfoods.com>
2022-10-21 10:06:00 -0700

    feat: include home-manager configuration

---
+++ nix/home-manager/nvim.nix
@@ -0,0 +1,58 @@
+pkgs: {
+  enable = true;
+  coc = {
+    enable = true;
+    pluginConfig = ''
+      
+    '';
+    settings = {
+        
+    };
+  };
+  withPython3 = true;
+  extraConfig = ''
+    luafile $HOME/.config/nixpkgs/settings.lua
+  '';
+  extraLuaPackages = ps: with ps; [];
+  extraPackages = [];
+  extraPython3Packages = ps: with; [];
+  plugins = with pkgs.vimPlugins; [
+    ale
+    coc-denite
+    coc-json
+    # coc-marketplace
+    # coc-sh
+    # coc-docker
+    coc-nvim
+    coc-pyright
+    coc-vimtex
+    csv-vim
+    ctrlp-vim
+    denite-nvim
+    dracula-vim
+    jsonc-vim
+    nerdtree
+    taboo-vim
+    tagbar
+    terminus
+    tmuxline-vim
+    undotree
+    vimtex
+    vim-airline
+    vim-airline-themes
+    # vim-ctrlspace
+    vim-devicons
+    vim-flog
+    vim-fugitive
+    vim-gitgutter
+    vim-lastplace
+    vim-nix
+    vim-startify
+    vim-surround
+    # (nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars))
+    # yankring
+  ];
+  viAlias = false;
+  vimAlias = false;
+  vimdiffAlias = true;
+}
