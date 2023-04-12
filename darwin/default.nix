59b00750406ac539542de87da27a6378b11c3022
Tristan Schrader <tristanschrader@proton.me>
2023-04-11 17:21:18 -0700

    dev: rearrange and simplify/standardize config

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,5 +1,6 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, nix-doom-emacs, home-manager }:
 {
+  imports = [ home-manager.darwinModules.home-manager ];
   environment.systemPackages = [ ];
   services.nix-daemon.enable = true;
   programs.zsh.enable = true;
