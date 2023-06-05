3f4518250ef9aa7324b4ed0ab3cf541cdc4e93d6
Tristan Schrader <t0rdos@pm.me>
2023-06-04 20:28:35 -0700

    include script to install spicetify

--- lib/install.sh
+++ lib/install.sh
@@ -3,4 +3,8 @@ if [[ -e /etc/nixos ]]; then
   sudo nixos-rebuild test --flake '.#'
 else
   nix run "$(pwd)#homeConfigurations.tristanschrader.activationPackage"
+  # Install other things that can't be managed by nix
+  # SPICETIFY
+  # It doesn't seem like this tool is mature enough in nix to work properly (spicetify-cli can't detect anything).
+# [ ! -d ~/.config/spicetify ] && curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
 fi
