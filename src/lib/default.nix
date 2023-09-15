9bcbe10f8b98ca0cc54e6e8e0d6ebcc7a484de3c
Tristan Schrader <t0rdos@pm.me>
2023-09-15 00:20:15 -0700

    add template substitution command change template

--- src/lib/default.nix
+++ src/lib/default.nix
@@ -7,4 +7,10 @@
       output_f = pkgs.runCommand "to-yaml" { nativeBuildInputs = [ pkgs.remarshal ]; } command;
       values = [ (builtins.readFile output_f) ];
     in values;
+  subTemplateCmds = { template, cmds ? {} }:
+    let
+      contents_old = builtins.readFile template;
+      cmds_sub_fmt = map (cmd: "\\${cmd}") (builtins.attrNames cmds);
+      contents_new = builtins.replaceStrings cmds_sub_fmt (builtins.attrValues cmds) contents_old;
+    in contents_new;
 }
