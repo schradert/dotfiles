4cafbd1422094b54f98853c92323150915bf4350
Tristan Schrader <tristan@climaxfoods.com>
2023-09-15 00:25:02 -0700

    reorganize repo a little

--- lib/tf-cmd.sh
+++
@@ -1,9 +0,0 @@
-#!@bash@
-
-build_d="$(pwd)/build"
-[[ ! -d $build_d ]] && mkdir "$build_d"
-result="$build_d/config.tf.json"
-[[ -e $result ]] && rm -f "$result"
-cp @config@ "$result"
-@terraform@ -chdir=build init
-@terraform@ -chdir=build @command@
