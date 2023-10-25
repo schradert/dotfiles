a98749aa118bf9d6fcd38f45d3ff65e0ea4838a3
Tristan Schrader <tristan@climaxfoods.com>
2023-10-24 20:25:24 -0700

    configure ssh-deploy for repo to sync with sirver

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . (
+  (ssh-deploy-root-local . "/Users/tristanschrader/Code/Personal/dev/dotfiles/")
+  (ssh-deploy-root-remote . "/ssh:sirver:/home/tristan/dotfiles/")
+  (ssh-deploy-on-explicit-save . 1)
+  (ssh-deploy-force-on-explicit-save . 1)
+)))
