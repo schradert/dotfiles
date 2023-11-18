7614968616deab16f367895b8178309a01998338
Tristan Schrader <tristan@climaxfoods.com>
2023-11-18 11:57:23 -0800

    upgrade sops with overlay to fix XDG_CONFIG on macos

--- src/overlays/external.nix
+++ src/overlays/external.nix
@@ -21,4 +21,21 @@ final: prev: {
     });
   };
   # Upgrading sops because: https://github.com/getsops/sops/issues/1263
+  # Overriding go modules requires overriding the buildGoModule function instead of attributes directly.
+  # Follow the discussion and activity below:
+  # [Issue](https://github.com/NixOS/nixpkgs/issues/86349)
+  # [PR: buildGoModule](https://github.com/NixOS/nixpkgs/pull/225051)
+  # [PR: lib.extendMkDerivation](https://github.com/NixOS/nixpkgs/pull/234651)
+  sops = prev.sops.override {
+    buildGoModule = args: prev.buildGoModule (args // rec {
+      version = "3.8.1";
+      src = prev.fetchFromGitHub {
+        owner = "mozilla";
+        repo = args.pname;
+        rev = "v${version}";
+        sha256 = "4K09wLV1+TvYTtvha6YyGhjlhEldWL1eVazNwcEhi3Q=";
+      };
+      vendorSha256 = "iRgLspYhwSVuL0yarPdjXCKfjK7TGDZeQCOcIYtNvzA=";
+    });
+  };
 }
