3effdeb46b6bd7b4dea27928a0d32e249de3219b
Tristan Schrader <tristan@climaxfoods.com>
2023-11-20 10:35:29 -0800

    redesign people module with me/my attributes
    also changed the api for passing in public keys to include a name
    associated with each

--- src/users/config.nix
+++ src/users/config.nix
@@ -1,12 +1,12 @@
-{
-  myself = "tristan";
-  users = {
-    tristan = {
-      name = "Tristan Schrader";
-      email = "tristan@climaxfoods.com";
-      sshKeys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBRaIPhp5LExmqK7KECgbqdTY3goyUfNgKjKD9Wkz3dN"
-      ];
+{...}: {
+  people = {
+    me = "tristan";
+    users = {
+      tristan = {
+        name = "Tristan Schrader";
+        email = "tristan@climaxfoods.com";
+        sshKeys.github.public = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBRaIPhp5LExmqK7KECgbqdTY3goyUfNgKjKD9Wkz3dN";
+      };
     };
   };
 }
