33f4b1976b839039bd647a16ad1c1b7aecfa5174
Tristan Schrader <t0rdos@pm.me>
2023-11-20 11:41:58 -0800

    update github ssh public key

--- src/users/config.nix
+++ src/users/config.nix
@@ -5,7 +5,7 @@
       tristan = {
         name = "Tristan Schrader";
         email = "tristan@climaxfoods.com";
-        sshKeys.github.public = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBRaIPhp5LExmqK7KECgbqdTY3goyUfNgKjKD9Wkz3dN";
+        sshKeys.github.public = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBU/var4dzyl3BvVvN/e+T6JjnptZVhLpLemn6UaUcm";
       };
     };
   };
