1efe02db361dbfbfe480aae8a85df2f4046d996a
Tristan Schrader <tristan@climaxfoods.com>
2023-12-06 09:56:35 -0800

    port over basic flux deployment in terraform

--- flake.nix
+++ flake.nix
@@ -136,6 +136,34 @@
               };
               provider.helm = {inherit (config.provider) kubernetes;};
             })
+            {
+              terraform.required_providers = {
+                github.source = "integrations/github";
+                github.version = "5.42.0";
+                flux.source = "fluxcd/flux";
+                flux.version = "1.1.2";
+              };
+              resource.tls_private_key.flux = {
+                algorithm = "ECDSA";
+                ecdsa_curve = "P256";
+              };
+              resource.github_repository_deploy_key.flux = {
+                key = "\${ tls_private_key.flux.public_key_openssh }";
+                read_only = false;
+                repository = "backbone";
+                title = "Flux";
+              };
+              provider.flux = {
+                kubernetes = {};
+                git.url = "ssh://git@github.com/schradert/dotfiles";
+                git.branch = "trunk";
+                git.ssh = {
+                  username = "git";
+                  private_key = "\${ tls_private_key.flux.private_key_pem }";
+                };
+              };
+              resource.flux_bootstrap_git.prod = {path = "src/nux/clusters/prod";};
+            }
           ];
         };
       };
