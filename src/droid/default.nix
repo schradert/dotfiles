379a22a26d64fe038ec03f93af7e339575f1d67e
Tristan Schrader <tristan@climaxfoods.com>
2023-12-11 22:03:43 -0800

    define basic nix-on-droid config for boox

---
+++ src/droid/default.nix
@@ -0,0 +1,20 @@
+{self, ...}:
+{
+  flake = {
+    droidModules.common = {
+      environment.etcBackupExtension = ".bak";
+      home-manager.backupFileExtension = "hm-bak";
+      home-manager.config = {
+        imports = [self.homeModules.common];
+        # TODO what's the right user name and location?
+        # home.username = "termux";
+        # home.userDirectory = "/data/data/com.termux.nix/files/home";
+      };
+      home-manager.useGlobalPkgs = true;
+      nix.extraOptions = ''
+        experimental-features = nix-command flakes
+      '';
+      system.stateVersion = "23.05";
+    };
+  };
+}
