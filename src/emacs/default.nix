c05bcf1df07582d9a2d9b318583063f2aa54314b
Tristan Schrader <t0rdos@pm.me>
2024-02-12 23:31:45 -0800

    finally get emacs to work
    unfortunately managing doom is too difficult with nix because there
    aren't enough variables to control to allow running doom emacs that
    don't overwrite nix controlled derivations

--- src/emacs/default.nix
+++ src/emacs/default.nix
@@ -1,77 +1,74 @@
-{inputs, ...}: {
+{inputs, nix, ...}: with nix; {
   flake.overlays.emacs = inputs.emacs-overlay.overlay;
   flake.homeModules.emacs = {
     config,
     flake,
     lib,
-    nix,
     pkgs,
     ...
-  }:
-    with lib;
-      mkMerge [
-        {
-          services.emacs = {
-            enable = mkDefault pkgs.stdenv.isLinux;
-            defaultEditor = config.dotfiles.editor == "emacs";
-            # package = inputs.emacs-overlay.packages.${system}.emacs-unstable-pgtk;
-            package = pkgs.emacs-unstable-pgtk;
-          };
-        }
-        (mkIf config.services.emacs.enable (mkMerge [
-          {
-            programs.zsh.initExtraLines = nix.toList "export PATH=\"$HOME/.config/emacs/bin:$PATH\"";
-            home = {
-              file.".doom.d/init.el".source = ./init.el;
-              file.".doom.d/config.org".source = ./config.org;
-              # TODO get elfeed working
-              packages = with pkgs; [
-                config.services.emacs.package
-
-                # dependencies
-                cargo
-                editorconfig-core-c
-                gopls
-                gotools
-                gomodifytags
-                gore
-                gotests
-                gnugrep
-                graphviz
-                haskellPackages.haskell-language-server
-                haskellPackages.hoogle
-                haskellPackages.cabal-install
-                imagemagick
-                ispell
-                isync
-                ktlint
-                mu
-                nil
-                nixfmt
-                nodePackages.js-beautify
-                nodePackages.stylelint
-                pandoc
-                pipenv
-                python311Packages.grip
-                python311Packages.isort
-                python311Packages.nose
-                python311Packages.pytest
-                rust-analyzer
-                rustc
-                shellcheck
-                taplo
-                sqls
-              ];
-            };
-          }
-          (mkIf config.programs.git.enable {
-            home.activation.doomInstallation = hm.dag.entryAfter ["linkGeneration"] ''
-              if [ ! -d "$XDG_CONFIG_HOME/emacs" ]; then
-                ${pkgs.git}/bin/git clone --depth 1 https://github.com/doomemacs/doomemacs "$XDG_CONFIG_HOME/emacs"
-                "$XDG_CONFIG_HOME/emacs/bin/doom" install
-              fi
-            '';
-          })
-        ]))
-      ];
+  }: {
+    config = lib.mkIf config.programs.emacs.enable (mkMerge [
+      {
+        programs.emacs.package = pkgs.emacs-unstable-pgtk;
+        home = let
+          emacs = "${config.xdg.configHome}/emacs";
+        in {
+          activation.doomInstallation = lib.hm.dag.entryAfter ["writeBoundary"] ''
+            export PATH=$PATH:${config.programs.emacs.package}/bin:${pkgs.git}/bin:${emacs}/bin
+            if [[ ! -d ${emacs} ]]; then
+              git clone --depth 1 https://github.com/doomemacs/doomemacs ${emacs}
+              doom install
+            else
+              doom sync
+            fi
+          '';
+          file.".doom.d/init.el".source = ./init.el;
+          file.".doom.d/config.org".source = ./config.org;
+          # TODO get elfeed working
+          packages = with pkgs; [
+            cargo
+            editorconfig-core-c
+            gopls
+            gotools
+            gomodifytags
+            gore
+            gotests
+            gnugrep
+            graphviz
+            haskellPackages.haskell-language-server
+            haskellPackages.hoogle
+            haskellPackages.cabal-install
+            imagemagick
+            ispell
+            isync
+            ktlint
+            mu
+            nil
+            nixfmt
+            nodePackages.js-beautify
+            nodePackages.stylelint
+            pandoc
+            pipenv
+            python311Packages.grip
+            python311Packages.isort
+            python311Packages.nose
+            python311Packages.pytest
+            rust-analyzer
+            rustc
+            shellcheck
+            taplo
+            sqls
+          ];
+          sessionPath = ["${emacs}/bin"];
+        };
+      }
+      (lib.mkIf pkgs.stdenv.isLinux {
+        services.emacs = {
+          enable = mkDefault true;
+          defaultEditor = config.dotfiles.editor == "emacs";
+          package = config.programs.emacs.package;
+        };
+      })
+    ]);
+  };
 }
