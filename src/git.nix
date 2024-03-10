f20ab67395d3e2682dc79fda00f8b7a5d65f6e2d
Tristan Schrader <t0rdos@pm.me>
2024-03-10 13:37:51 -0700

    Add some common git configuration

--- src/git.nix
+++ src/git.nix
@@ -13,10 +13,17 @@
         enable = true;
         userName = my.name;
         userEmail = my.profiles.${config.dotfiles.profile}.email;
+        delta.enable = true;
+        # TODO find out what delta options I like the most
+        # TODO how does this compare to diff-so-fancy or difftastic
+        delta.options = {};
+        # TODO should I use git large file storage or prefer a different strategy?
+        lfs.enable = false;
         extraConfig =
           {
             push.autoSetupRemote = true;
             color.status = "always";
+            init.defaultBranch = "trunk";
           }
           // mapAttrs (_: setAttrByPath ["user"]) my.accounts;
       };
@@ -29,6 +36,9 @@
           else "https";
         settings.aliases.co = "pr checkout";
       };
+      programs.git-cliff.enable = true;
+      # TODO figure out how to use git-cliff and find what settings I prefer
+      programs.git-cliff.settings = {};
       programs.zsh.initExtraLines = optional config.programs.git.enable ''
         # disable sort when completing `git checkout`
         zstyle ':completion:*:git-checkout:*' sort false
