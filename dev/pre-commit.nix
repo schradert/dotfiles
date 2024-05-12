b30ca45236f002e90b809a2ef007ee5bbc529016
Tristan Schrader <t0rdos@pm.me>
2024-05-12 13:57:13 -0700

    bump: canivete newest

--- dev/pre-commit.nix
+++ dev/pre-commit.nix
@@ -1,14 +1,16 @@
 {
   perSystem = {pkgs, ...}: {
-    canivete.pre-commit.shell.enable = true;
-    pre-commit.settings = {
-      excludes = ["old/.+" ".canivete/sops/default.yaml" ".canivete/sops/tristan"];
-      # TODO extract these tool configurations into options
-      hooks.lychee.settings.configPath = toString (pkgs.writers.writeTOML "lychee.toml" {exclude_path = ["programs/emacs/config.org"];});
-      # Used in vim configuration
-      hooks.typos.settings.configPath = toString (pkgs.writers.writeTOML "_typos.toml" {default.extend-words.enew = "enew";});
-      # zsh not really supported by shfmt
-      hooks.shfmt.excludes = ["programs/zsh/.p10k.zsh"];
+    canivete.pre-commit = {
+      languages.shell.enable = true;
+      settings = {
+        excludes = ["old/.+" ".canivete/sops/default.yaml" ".canivete/sops/tristan"];
+        # TODO extract these tool configurations into options
+        hooks.lychee.settings.configPath = toString (pkgs.writers.writeTOML "lychee.toml" {exclude_path = ["programs/emacs/config.org"];});
+        # Used in vim configuration
+        hooks.typos.settings.configPath = toString (pkgs.writers.writeTOML "_typos.toml" {default.extend-words.enew = "enew";});
+        # zsh not really supported by shfmt
+        hooks.shfmt.excludes = ["programs/zsh/.p10k.zsh"];
+      };
     };
   };
 }
