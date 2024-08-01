7b7f4cfe0367e9a375fc5aac530e5ef564755008
Tristan Schrader <t0rdos@pm.me>
2024-08-01 13:50:30 -0700

    refactor: adapt heliboard to new activation script

--- modules/programs/heliboard.nix
+++ modules/programs/heliboard.nix
@@ -1,20 +1,8 @@
-{nix, ...}: with nix; {
-  canivete.deploy.droid.modules.heliboard = {config, pkgs, ...}: let
-    inherit (config.dotfiles.programs) heliboard;
-  in {
-    config = mkIf heliboard.enable {environment.packages = [heliboard.package];};
-    options.dotfiles.programs.heliboard = {
-      enable = mkEnabledOption "HeliBoard keyboard application";
-      # TODO convert this to gradle build in nix
-      # NOTE how to use gradlew? do I need gradle2nix/v2? can I do it from scratch?
-      package = mkOption {
-        type = package;
-        description = "Package to include in environment";
-        default = pkgs.fetchurl {
-          url = "https://github.com/Helium314/HeliBoard/releases/download/v2.2/HeliBoard_2.2-release.apk";
-          hash = "sha256-GIHjs4nL363OMN4GAmOn38rRHT0NQ6RLb41952QePYA=";
-        };
-      };
+{
+  canivete.deploy.droid.homeModules.heliboard = {pkgs, ...}: {
+    dotfiles.programs.apk.heliboard.apk = pkgs.fetchurl {
+      url = "https://github.com/Helium314/HeliBoard/releases/download/v2.2/HeliBoard_2.2-release.apk";
+      hash = "sha256-GIHjs4nL363OMN4GAmOn38rRHT0NQ6RLb41952QePYA=";
     };
   };
 }
