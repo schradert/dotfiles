043cd94c584641febbcb113eccab9966e292b03b
Tristan Schrader <t0rdos@pm.me>
2024-08-01 11:54:48 -0700

    fix: temporarily disable graphical to speed up eval

--- builds/droid.nix
+++ builds/droid.nix
@@ -1,6 +1,6 @@
 {
   canivete.deploy.droid = {
-    nodes.boox.profiles.system.module.dotfiles.graphical.enable = true;
-    nodes.galaxy.profiles.system.module.dotfiles.graphical.enable = true;
+    nodes.boox.profiles.system.module = {};
+    nodes.galaxy.profiles.system.module = {};
   };
 }
