83e5bb211f131139e2e038b1d5bf8e9ce89355e0
Tristan Schrader <t0rdos@pm.me>
2024-08-24 14:25:25 -0700

    refactor: move build modules into /modules

--- modules/kubernetes.nix
+++ modules/kubernetes.nix
@@ -1,15 +1,89 @@
-{nix, ...}:
-with nix; {
-  perSystem.canivete.opentofu.workspaces.deploy.modules.k3s-token.resource.random_password.k3s-token.length = 21;
+{config, nix, ...}:
+with nix; let
+  inherit (config.dotfiles) domain;
+in {
+   perSystem = {
+    config,
+    inputs',
+    pkgs,
+    self',
+    system,
+    ...
+  }: {
+    options.dotfiles.nix2container = mkOption {
+      default = {};
+      type = attrsOf (submodule ({
+        name,
+        config,
+        ...
+      }: {
+        options = {
+          registry = mkOption {
+            type = str;
+            default = "harbor.${domain}";
+          };
+          repository = mkOption {
+            type = str;
+            default = name;
+          };
+          package = mkOption {
+            type = package;
+            default = self'.packages.${name} or pkgs.${name};
+          };
+          args = mkOption {
+            type = attrsOf anything;
+            default = {};
+          };
+          image = mkOption {
+            type = package;
+            default = pipe config.args [
+              (recursiveUpdate {
+                name = "${config.registry}/${config.repository}";
+                config.entrypoint = [(getExe config.package)];
+              })
+              inputs'.nix2container.packages.nix2container.buildImage
+            ];
+          };
+        };
+      }));
+    };
+    config.canivete.opentofu.workspaces.deploy.modules = {
+      k3s-token.resource.random_password.k3s-token.length = 21;
+      nix2container = {
+        config = mkMerge (flip mapAttrsToList config.dotfiles.nix2container (name: container: let
+          path = "dotfiles.${replaceStrings ["darwin"] ["linux"] system}.nix2container.${name}.image";
+        in {
+          resource.null_resource.kubernetes = {
+            depends_on = ["null_resource.${name}"];
+            provisioner.local-exec.environment = pipe ["tag" "registry" "repository" "fullRepository"] [
+              (flip genAttrs (attr: "\${ data.external.${name}.result.${attr} }"))
+              (mapAttrNames (key: "${toUpper name}_IMAGE_${toUpper key}"))
+            ];
+          };
+          data.external.${name}.program = pkgs.execBash ''
+            export \
+              drv=$(nix path-info --derivation .#${path}) \
+              tag=$(nix eval --raw .#${path}.imageTag) \
+              registry=${container.registry} \
+              repository=${container.repository} \
+              fullRepository=${container.registry}/${container.repository}
+            ${getExe pkgs.jq} --null-input '$ARGS.positional | map({(.):env[.]}) | add' --args drv tag registry repository fullRepository
+          '';
+          resource.null_resource.${name} = {
+            triggers.drv = "\${ data.external.${name}.result.drv }";
+            provisioner.local-exec.command = "nix run .#${path}.copyToRegistry";
+          };
+        }));
+      };
+    };
+  };
   canivete.deploy.nixos.modules.kubernetes = {
     config,
-    flake,
     pkgs,
     ...
   }: let
     cfg = config.dotfiles.kubernetes;
     cfg_k3s = config.services.k3s;
-    inherit (flake.config.dotfiles) domain;
   in {
     options.dotfiles.kubernetes = {
       enable = mkEnableOption "kubernetes as a service";
