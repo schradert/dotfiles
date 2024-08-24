38eb0acef642a453090b4283172da33abd38c122
Tristan Schrader <t0rdos@pm.me>
2024-08-23 22:39:52 -0700

    feat: android-integration settings

--- modules/profiles/droid.nix
+++ modules/profiles/droid.nix
@@ -47,6 +47,15 @@ with nix; {
         };
       };
       system.stateVersion = "23.05";
+      android-integration = {
+        am.enable = true;
+        termux-open.enable = true;
+        termux-setup-storage.enable = true;
+        termux-reload-settings.enable = true;
+        termux-wake-lock.enable = true;
+        xdg-open.enable = true;
+        unsupported.enable = true;
+      };
     };
   };
 }
