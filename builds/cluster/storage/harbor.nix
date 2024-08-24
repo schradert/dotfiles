ad3e60024ba4ed0e92454018fcf20ca0263b78f7
Tristan Schrader <t0rdos@pm.me>
2024-08-24 00:16:20 -0700

    feat: harbor

---
+++ builds/cluster/storage/harbor.nix
@@ -0,0 +1,16 @@
+{
+  # https://github.com/goharbor/harbor
+  # https://goharbor.io/docs/2.11.0/install-config/harbor-ha-helm/
+  # TODO package harbor with nix
+  perSystem.dotfiles.helm.harbor = {
+    namespace = "storage";
+    chart = {
+      repo = "https://helm.goharbor.io";
+      chart = "harbor";
+      version = "1.15.0";
+      sha256 = "gj3Z00+ZwznFZP84ezl6lgEeAfBso/2+M/OfCYWlC0Y=";
+    };
+    # Has to be a string!
+    values.trivy.resources.limits.cpu = "1";
+  };
+}
