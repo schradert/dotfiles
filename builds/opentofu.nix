998a57f8ee11a5c4593a18baa6ee8d58e4ef502d
Tristan Schrader <t0rdos@pm.me>
2024-08-23 22:56:41 -0700

    feat: opentofu module for passwords
    kubernetes deployment will await every password

---
+++ builds/opentofu.nix
@@ -0,0 +1,17 @@
+{self, nix, ...}: with nix; {
+  flake.lib.nixToEnv = flip pipe [
+    toUpper
+    (replaceStrings ["." "-"] ["__" "_"])
+  ];
+  perSystem = {config, ...}: {
+    options.dotfiles.opentofu.passwords = mkOption {
+      type = attrsOf (attrsOf anything);
+      default = {};
+    };
+    config.canivete.opentofu.workspaces.${config.canivete.kubenix.clusters.prod.opentofuWorkspace}.modules.passwords.resource = mkMerge (flip mapAttrsToList config.dotfiles.opentofu.passwords (name: cfg: {
+      random_password.${name} = cfg;
+      null_resource.kubernetes.depends_on = ["random_password.${name}"];
+      null_resource.kubernetes.provisioner.local-exec.environment.${self.lib.nixToEnv name} = "\${ random_password.${name}.result }";
+    }));
+  };
+}
