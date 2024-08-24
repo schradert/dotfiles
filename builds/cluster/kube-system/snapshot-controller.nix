491a30910d91a28323baae8b7ccf82157de69ee8
Tristan Schrader <t0rdos@pm.me>
2024-08-24 00:21:12 -0700

    feat: snapshot-controller

---
+++ builds/cluster/kube-system/snapshot-controller.nix
@@ -0,0 +1,13 @@
+{
+  perSystem.dotfiles.helm.snapshot-controller = {
+    namespace = "kube-system";
+    chart = {
+      repo = "https://piraeus.io/helm-charts";
+      chart = "snapshot-controller";
+      version = "3.0.5";
+      sha256 = "vEdvr5sz5C8jOL4CgteS78Tg48R2k81rrnWK6KnzDSE=";
+    };
+    values.controller.serviceMonitor.create = true;
+    values.webhook.enabled = false;
+  };
+}
