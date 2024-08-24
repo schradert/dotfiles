12c22cfed7c933d5d42d498cd04299de60933451
Tristan Schrader <t0rdos@pm.me>
2024-08-24 00:17:12 -0700

    feat: cert-manager
    seems better maintained than traefik

---
+++ builds/cluster/security/cert-manager.nix
@@ -0,0 +1,49 @@
+{config, nix, ...}: let
+  inherit (config.canivete.people.my.profiles.default) email;
+  inherit (config.dotfiles) domain;
+  domainName = nix.replaceStrings ["."] ["-"] domain;
+  mkClusterIssuer = name: server: {
+    spec.acme = {
+      inherit server email;
+      privateKeySecretRef = {inherit name;};
+      solvers = nix.toList {
+        dns01.cloudflare = {
+          inherit email;
+          apiKeySecretRef.name = "cloudflare";
+          apiKeySecretRef.key = "cloudflare_pat";
+        };
+        selector.dnsZones = [domain];
+      };
+    };
+  };
+in {
+  # https://github.com/cert-manager/cert-manager
+  perSystem.dotfiles.helm.cert-manager = {
+    namespace = "security";
+    chart = {
+      repo = "https://charts.jetstack.io";
+      chart = "cert-manager";
+      version = "1.15.3";
+      sha256 = "BY20Xn1EZimdwhhT2OL64Hhz42RD9OHlddOzRg0iHNw=";
+    };
+    values = {
+      crds.enabled = true;
+      dns01RecursiveNameservers = "https://1.1.1.1:443/dns-query,https://1.0.0.1:443/dns-query";
+      dns01RecursiveNameserversOnly = true;
+      prometheus.enabled = true;
+      prometheus.servicemonitor.enabled = true;
+    };
+    resources.secrets.cloudflare.stringData.cloudflare_pat = nix.vals.sops "default.yaml#/cloudflare_pat";
+    resources.clusterissuers = {
+      letsencrypt-production = mkClusterIssuer "letsencrypt-production" "https://acme-v02.api.letsencrypt.org/directory";
+      letsencrypt-staging = mkClusterIssuer "letsencrypt-staging" "https://acme-staging-v02.api.letsencrypt.org/directory";
+    };
+    resources.certificates.${domainName}.spec = {
+      secretName = "${domainName}-tls";
+      issuerRef.name = "letsencrypt-staging";
+      issuerRef.kind = "ClusterIssuer";
+      commonName = domain;
+      dnsNames = [domain "*.${domain}"];
+    };
+  };
+}
