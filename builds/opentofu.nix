d1e1a03f33df113b4786e7e7e6a12a13e169cf61
Tristan Schrader <t0rdos@pm.me>
2024-08-24 12:31:55 -0700

    fix: include opentofu/random for passwords

--- builds/opentofu.nix
+++ builds/opentofu.nix
@@ -13,10 +13,13 @@ with nix; {
       type = attrsOf (attrsOf anything);
       default = {};
     };
-    config.canivete.opentofu.workspaces.${config.canivete.kubenix.clusters.prod.opentofuWorkspace}.modules.passwords.resource = mkMerge (flip mapAttrsToList config.dotfiles.opentofu.passwords (name: cfg: {
-      random_password.${name} = cfg;
-      null_resource.kubernetes.depends_on = ["random_password.${name}"];
-      null_resource.kubernetes.provisioner.local-exec.environment.${self.lib.nixToEnv name} = "\${ random_password.${name}.result }";
-    }));
+    config.canivete.opentofu.workspaces.${config.canivete.kubenix.clusters.prod.opentofuWorkspace} = {
+      plugins = ["opentofu/random"];
+      modules.passwords.resource = mkMerge (flip mapAttrsToList config.dotfiles.opentofu.passwords (name: cfg: {
+        random_password.${name} = cfg;
+        null_resource.kubernetes.depends_on = ["random_password.${name}"];
+        null_resource.kubernetes.provisioner.local-exec.environment.${self.lib.nixToEnv name} = "\${ random_password.${name}.result }";
+      }));
+    };
   };
 }
