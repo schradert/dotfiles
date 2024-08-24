bd430d5b677d70ad3e37dd96cb9620e18dcc2306
Tristan Schrader <t0rdos@pm.me>
2024-08-24 00:21:20 -0700

    feat: spegel

---
+++ builds/cluster/kube-system/spegel.nix
@@ -0,0 +1,21 @@
+
+{
+  # https://github.com/spegel-org/spegel
+  perSystem.dotfiles.helm.spegel = {
+    namespace = "kube-system";
+    chart = {
+      chartUrl = "oci://ghcr.io/spegel-org/helm-charts/spegel";
+      chart = "spegel";
+      version = "v0.0.23";
+      sha256 = "JJBUCtvwSffZ1BKmiUD2asTdSXNsIIYz68ceCeSSMGI=";
+    };
+    values = {
+      grafanaDashboard.enabled = true;
+      serviceMonitor.enabled = true;
+      spegel.appendMirrors = true;
+      # TODO is this what I need to do for podman? why under 30000?
+      spegel.containerdRegistryConfigPath = "/etc/cri/conf.d/hosts";
+      service.registry.hostPort = 29999;
+    };
+  };
+}
