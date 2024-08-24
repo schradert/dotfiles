70e9102ea695c3ceda6b26c60f7867b94c77e31d
Tristan Schrader <t0rdos@pm.me>
2024-08-23 23:39:40 -0700

    feat: generate namespaces for every directory

--- builds/cluster/default.nix
+++ builds/cluster/default.nix
@@ -1,3 +1,13 @@
-{config, ...}: {
-  perSystem.canivete.kubenix.clusters.prod.deploy.fetchKubeconfig = "ssh sirver sudo k3s kubectl config view --raw | sed 's/127\.0\.0\.1/${config.dotfiles.domain}/'";
+{config, nix, ...}: with nix; let
+  command = "ssh ${config.canivete.root} sudo k3s kubectl config view --raw | sed 's/127\.0\.0\.1/${config.dotfiles.domain}/'";
+in {
+  perSystem.canivete.kubenix.clusters.prod = {
+    deploy.fetchKubeconfig = command;
+    modules.namespaces.kubernetes.resources.namespaces = pipe ./. [
+      filesets.dirs
+      (map baseNameOf)
+      (flip genAttrs (_: {}))
+      (flip removeAttrs ["kube-system"])
+    ];
+  };
 }
