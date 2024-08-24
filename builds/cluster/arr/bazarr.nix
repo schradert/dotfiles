ea7793bc5d767f2a6f2b20fd2d418142ac087d55
Tristan Schrader <t0rdos@pm.me>
2024-08-24 00:33:16 -0700

    feat: bazarr

---
+++ builds/cluster/arr/bazarr.nix
@@ -0,0 +1,31 @@
+{config, nix, ...}: with nix; {
+  # TODO plex token?
+  # TODO should I use subcleaner https://github.com/KBlixt/subcleaner
+  perSystem.dotfiles.nix2container.bazarr = {};
+  perSystem.dotfiles.helm.bazarr = {
+    namespace = "arr";
+    values = {
+      controllers.bazarr.containers.bazarr = {
+        image.repository = "ref+envsubst://BAZARR_IMAGE_FULLREPOSITORY+";
+        image.tag = "ref+envsubst://BAZARR_IMAGE_TAG";
+        envFrom = toList {secret = "bazarr";};
+        probes.liveness.enabled = true;
+        probes.readiness.enabled = true;
+        probes.startup.enabled = true;
+      };
+      service.bazarr.controller = "bazarr";
+      service.bazarr.ports.http.port = 6767;
+      ingress.bazarr.className = "internal";
+      ingress.bazarr.hosts = toList {
+        host = "bazarr.${config.dotfiles.domain}";
+        paths = toList {
+          path = "/";
+          service.identifier = "bazarr";
+          service.port = "http";
+        };
+      };
+      secrets.bazarr.enabled = true;
+      secrets.bazarr.stringData.PLEX_TOKEN = "";
+    };
+  };
+}
