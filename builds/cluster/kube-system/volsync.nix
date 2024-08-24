254bba6bb7988aab4a08bd5b9065e6c078713d01
Tristan Schrader <t0rdos@pm.me>
2024-08-24 00:20:08 -0700

    feat: basic volsync

---
+++ builds/cluster/kube-system/volsync.nix
@@ -0,0 +1,13 @@
+
+{
+  perSystem.dotfiles.helm.volsync = {
+    namespace = "kube-system";
+    chart = {
+      repo = "https://backube.github.io/helm-charts";
+      chart = "volsync";
+      version = "0.10.0";
+      sha256 = "LnAeoCsA/oHoR0jZWVb9dw0YXuThS3LqoX40Q/8Lh9c=";
+    };
+    values.metrics.disableAuth = true;
+  };
+}
