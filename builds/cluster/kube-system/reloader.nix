8c3d2b15b90d7598d782d1bb69b1d4f148952857
Tristan Schrader <t0rdos@pm.me>
2024-08-24 00:21:05 -0700

    feat: reloader

---
+++ builds/cluster/kube-system/reloader.nix
@@ -0,0 +1,18 @@
+{
+  # https://github.com/stakater/Reloader
+  perSystem.dotfiles.helm.reloader = {
+    namespace = "kube-system";
+    chart = {
+      chartUrl = "oci://ghcr.io/stakater/charts/reloader";
+      chart = "reloader";
+      version = "1.0.121";
+      sha256 = "ioCx1TxXSDCq6J06AZZecIsO25vjokjSyA9CFUwBJfw=";
+    };
+    values = {
+      fullnameOverride = "reloader";
+      reloader.readOnlyRootFileSystem = true;
+      reloader.podMonitor.enabled = true;
+      reloader.podMonitor.namespace = "kube-system";
+    };
+  };
+}
