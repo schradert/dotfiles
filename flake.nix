d4b6b7c954e1fe952562925b1c5414cca2d34f37
Tristan Schrader <t0rdos@pm.me>
2024-08-23 23:48:40 -0700

    fix: more cleanup
    expose dotfiles to flake level and remove broken deployments

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
     inputs.canivete.lib.mkFlake {
       inherit inputs;
       everything = [./modules ./builds];
-    } ({nix, ...}:
+    } ({config, nix, ...}:
       with nix; {
         dotfiles.domain = "trdos.me";
         canivete.root = "sirver";
@@ -62,23 +62,23 @@
             "steam"
             "steamdeck-hw-theme"
           ];
-        perSystem = {pkgs, ...}: {
-          canivete.pre-commit = {
-            languages.shell.enable = true;
-            settings = {
-              excludes = [".canivete/sops/.+"];
-              # TODO extract these tool configurations into options
-              hooks.lychee.settings.configPath = toString (pkgs.writers.writeTOML "lychee.toml" {
-                exclude_path = ["^\./modules/programs/emacs/config\.org$"];
-                # This helm repository doesn't have parent pages
-                exclude = ["https://seaweedfs.github.io/seaweedfs/helm"];
-              });
-              # Used in vim configuration
-              hooks.typos.settings.configPath = toString (pkgs.writers.writeTOML "_typos.toml" {default.extend-words.enew = "enew";});
-              # zsh not really supported by shfmt
-              hooks.shfmt.excludes = ["programs/zsh/.p10k.zsh"];
-            };
+        perSystem = {config, pkgs, ...}: {
+          packages.default = pkgs.wrapFlags config.packages.opentofu "--add-flags \"deploy\"";
+          canivete.pre-commit.languages.shell.enable = true;
+          canivete.pre-commit.settings = {
+            excludes = [".canivete/sops/.+"];
+            # TODO extract these tool configurations into options
+            hooks.lychee.settings.configPath = toString (pkgs.writers.writeTOML "lychee.toml" {
+              exclude_path = ["^\./modules/programs/emacs/config\.org$"];
+              # This helm repository doesn't have parent pages
+              exclude = ["https://seaweedfs.github.io/seaweedfs/helm"];
+            });
+            # Used in vim configuration
+            hooks.typos.settings.configPath = toString (pkgs.writers.writeTOML "_typos.toml" {default.extend-words.enew = "enew";});
+            # zsh not really supported by shfmt
+            hooks.shfmt.excludes = ["programs/zsh/.p10k.zsh"];
           };
         };
+        flake.dotfiles = mapAttrs (_: getAttr "dotfiles") config.allSystems;
       });
 }
