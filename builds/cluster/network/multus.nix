f2061bd6c016814e621821eb49f2f39c82b41757
Tristan Schrader <t0rdos@pm.me>
2024-08-24 00:02:30 -0700

    feat: [wip] placeholder multus

---
+++ builds/cluster/network/multus.nix
@@ -0,0 +1,17 @@
+# NOTE https://github.com/bjw-s/home-ops/blob/main/kubernetes/main/apps/network/multus/ks.yaml
+#
+{
+  perSystem = {pkgs, ...}: {
+    dotfiles.helm.multus = {
+      namespace = "network";
+      # resources.imports = [
+      #   (pkgs.fetchurl {
+      #     url = "https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/v4.1.0/deployments/multus-daemonset.yml";
+      #     sha256 = "";
+      #   })
+      # ];
+      # TODO implement network settings
+      values = {};
+    };
+  };
+}
