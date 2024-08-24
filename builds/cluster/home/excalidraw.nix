9808012a804ac6e027648088739ade76d302769d
Tristan Schrader <t0rdos@pm.me>
2024-08-24 01:15:43 -0700

    feat: excalidraw

---
+++ builds/cluster/home/excalidraw.nix
@@ -0,0 +1,14 @@
+{config, ...}: let
+  inherit (config.dotfiles) domain;
+in {
+  perSystem.canivete.kubenix.clusters.prod.modules.excalidraw = {helm, nix, ...}: {
+    kubernetes.helm.releases.excalidraw = {
+      chart = helm.fetch {
+        repo = "https://gitlab.com/api/v4/projects/43892189/packages/helm/stable";
+        chart = "excalidraw";
+        version = "0.1.4";
+        sha256 = "mFTSDKfYfGujNizrKao7PdsHYsKuDd81lrDb/BBRcAQ=";
+      };
+    };
+  };
+}
