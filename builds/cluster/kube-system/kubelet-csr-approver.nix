f1fe581c15e93e0dc47086faa5255b6913e8b4a9
Tristan Schrader <t0rdos@pm.me>
2024-08-24 00:20:47 -0700

    feat: csr-approver

---
+++ builds/cluster/kube-system/kubelet-csr-approver.nix
@@ -0,0 +1,23 @@
+{nix, self, ...}: with nix; {
+  # [ ] [kubelet-csr-approver](https://github.com/postfinance/kubelet-csr-approver)
+  perSystem.dotfiles.helm.kubelet-csr-approver = {
+    namespace = "kube-system";
+    chart = {
+      repo = "https://postfinance.github.io/kubelet-csr-approver";
+      chart = "kubelet-csr-approver";
+      version = "1.2.2";
+      sha256 = "J+/6oRReQrrGsHEWmfCnxtMNpAgcv8nrkn9L/wenmQM=";
+    };
+    values = {
+      providerRegex = pipe self.nixosConfigurations [
+        (filterAttrs (_: cfg: cfg.config.dotfiles.kubernetes.enable))
+        attrNames
+        (concatStringsSep "|")
+        (str: "^(${str})$")
+      ];
+      bypassDnsResolution = true;
+      metrics.enable = true;
+      metrics.serviceMonitor.enabled = true;
+    };
+  };
+}
