56a4c674521cb79a6be96f4d4406e6d0bd35008b
Tristan Schrader <t0rdos@pm.me>
2024-09-24 23:33:10 -0700

    feat: logical volumes on nixos with disko
    these are very useful for dynamically resizing cluster storage.
    unfortunately it required refactoring because disko is slightly broken
    and doesn't allow merging across modules exactly.

--- builds/nodes/nixos.nix
+++ builds/nodes/nixos.nix
@@ -2,90 +2,159 @@
   config,
   nix,
   ...
-}: let
+}: with nix; let
   inherit (config.canivete) root;
+  sshOptions = ["ProxyJump=${config.dotfiles.domain}"];
+  # Disko does not actually merge NixOS modules (major bummer!)
+  # I have to use recursiveUpdate within the same module for all settings to be detected and used
+  # NOTE https://github.com/nix-community/disko/issues/678
+  # NOTE https://github.com/NixOS/nixpkgs/pull/284551
+  disko.devices.disk.base = {
+    device = "/dev/sda";
+    type = "disk";
+    content.type = "gpt";
+    content.partitions = {
+      ESP = {
+        priority = 1;
+        type = "EF00";
+        size = "500M";
+        content.type = "filesystem";
+        content.format = "vfat";
+        content.mountpoint = "/boot";
+      };
+      root = {
+        priority = 2;
+        end = "-1G";
+        content.type = "filesystem";
+        content.format = "ext4";
+        content.mountpoint = "/";
+      };
+      swap = {
+        size = "100%";
+        content.type = "swap";
+        content.discardPolicy = "both";
+        content.resumeDevice = true;
+      };
+    };
+  };
+  lvmDisko = recursiveUpdate disko {
+    devices.disk.base = {
+      content.partitions.k8s = {
+        priority = 3;
+        end = "-1G";
+        content.type = "lvm_pv";
+        content.vg = "k8s";
+      };
+    };
+    devices.lvm_vg.k8s.type = "lvm_vg";
+  };
 in {
+  # TODO why the fuck do I have to keep doing this?
+  perSystem.canivete.opentofu.workspaces.deploy.modules.install-override.resource.null_resource = {
+    nixos_axolotl_system_install.provisioner.local-exec.command = mkForce "echo";
+    nixos_bonobo_system_install.provisioner.local-exec.command = mkForce "echo";
+    nixos_chinchilla_system_install.provisioner.local-exec.command = mkForce "echo";
+    nixos_dingo_system_install.provisioner.local-exec.command = mkForce "echo";
+    nixos_octopus_system_install.provisioner.local-exec.command = mkForce "echo";
+  };
   canivete.deploy.nixos.nodes = {
     sirver = {
       install.host = "192.168.50.23";
-      install.sshOptions = nix.mkForce [];
-      target.sshOptions = ["ProxyJump=${config.dotfiles.domain}"];
+      install.sshOptions = mkForce ["User=root"];
+      target.sshOptions = sshOptions;
       profiles.system.module = {
         dotfiles.kubernetes.enable = true;
         dotfiles.kubernetes.root = true;
         dotfiles.devops.enable = true;
-        disko.devices.disk.base.device = "/dev/disk/by-id/scsi-361866da05a3c260002ecf1a16170342b";
+        disko = recursiveUpdate lvmDisko {
+          devices.disk.base = {
+            device = "/dev/disk/by-id/scsi-361866da05a3c260002ecf1a16170342b";
+            content.partitions.root.end = "-1T";
+          };
+        };
         boot.initrd.availableKernelModules = ["ehci_pci" "megaraid_sas" "usbhid"];
         boot.kernelModules = ["kvm-intel"];
-        home-manager.sharedModules = nix.toList {home.stateVersion = "24.05";};
-        system.stateVersion = "24.05";
       };
     };
     axolotl = {
       install.host = "192.168.50.250";
       build.host = root;
-      build.sshOptions = ["ProxyJump=${config.dotfiles.domain}"];
+      build.sshOptions = sshOptions;
       profiles.system.sshProtocol = "ssh";
       profiles.system.module = {
         dotfiles.graphical.enable = true;
+        dotfiles.kubernetes.enable = true;
         boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "rtsx_pci_sdmmc"];
-        disko.devices.disk.base.device = "/dev/disk/by-id/nvme-SPCC_M.2_PCIE_SSD_30012119169";
-        powerManagement.cpuFreqGovernor = "powersave";
-        home-manager.sharedModules = nix.toList {
-          dotfiles.macchina.networkInterface = "enp0s31f6";
-          home.stateVersion = "24.05";
+        disko = recursiveUpdate lvmDisko {
+          devices.disk.base = {
+            device = "/dev/disk/by-id/nvme-SPCC_M.2_PCIE_SSD_30012119169";
+            content.partitions.root.end = "-101G";
+          };
         };
-        system.stateVersion = "24.05";
+        powerManagement.cpuFreqGovernor = "powersave";
+        home-manager.sharedModules = toList {dotfiles.macchina.networkInterface = "enp0s31f6";};
       };
     };
     bonobo = {
       install.host = "192.168.50.142";
-      build.sshOptions = ["ProxyJump=${config.dotfiles.domain}"];
+      build.sshOptions = sshOptions;
       profiles.system.module = {
         dotfiles.kubernetes.enable = true;
         boot.initrd.availableKernelModules = ["xhci_pci" "usbhid"];
-        disko.devices.disk.base.device = "/dev/disk/by-id/ata-Micron_1100_SATA_256GB_165015496CBD";
-        home-manager.sharedModules = nix.toList {home.stateVersion = "24.05";};
-        system.stateVersion = "24.05";
+        disko = recursiveUpdate lvmDisko {
+          devices.disk.base = {
+            device = "/dev/disk/by-id/ata-Micron_1100_SATA_256GB_165015496CBD";
+            content.partitions.root.end = "-51G";
+          };
+        };
       };
     };
     chinchilla = {
       install.host = "192.168.50.85";
-      build.sshOptions = ["ProxyJump=${config.dotfiles.domain}"];
+      build.sshOptions = sshOptions;
       profiles.system.module = {
         dotfiles.kubernetes.enable = true;
         boot.initrd.availableKernelModules = ["xhci_pci" "usbhid"];
-        disko.devices.disk.base.device = "/dev/disk/by-id/ata-MTFDDAK256TBN-1AR1ZABHA_UGXVK01J7BDCER";
-        home-manager.sharedModules = nix.toList {home.stateVersion = "24.05";};
-        system.stateVersion = "24.05";
+        disko = recursiveUpdate lvmDisko {
+          devices.disk.base = {
+            device = "/dev/disk/by-id/ata-MTFDDAK256TBN-1AR1ZABHA_UGXVK01J7BDCER";
+            content.partitions.root.end = "-51G";
+          };
+        };
       };
     };
     dingo = {
       install.host = "192.168.50.105";
-      build.sshOptions = ["ProxyJump=${config.dotfiles.domain}"];
+      build.sshOptions = sshOptions;
       profiles.system.module = {
         dotfiles.kubernetes.enable = true;
         boot.initrd.availableKernelModules = ["xhci_pci" "usbhid"];
-        disko.devices.disk.base.device = "/dev/disk/by-id/ata-LITEON_IT_LCS-256L9S_SD0E97900L2TH61100DL";
-        home-manager.sharedModules = nix.toList {home.stateVersion = "24.05";};
-        system.stateVersion = "24.05";
+        disko = recursiveUpdate lvmDisko {
+          devices.disk.base = {
+            device = "/dev/disk/by-id/ata-LITEON_IT_LCS-256L9S_SD0E97900L2TH61100DL";
+            content.partitions.root.end = "-51G";
+          };
+        };
       };
     };
     octopus = {
       install.host = "192.168.50.53";
-      target.sshOptions = ["ProxyJump=${config.dotfiles.domain}"];
+      target.sshOptions = sshOptions;
       profiles.system.module = {
         dotfiles.kubernetes.enable = true;
         boot.initrd.availableKernelModules = ["ehci_pci" "megaraid_sas" "usbhid" "sr_mod"];
         boot.kernelModules = ["kvm-intel"];
-        disko.devices.disk.base.device = "/dev/disk/by-id/scsi-36b82a720cf60ce002a80577e12e4a1a7";
-        home-manager.sharedModules = nix.toList {home.stateVersion = "24.05";};
-        system.stateVersion = "24.05";
+        disko = recursiveUpdate lvmDisko {
+          devices.disk.base = {
+            device = "/dev/disk/by-id/scsi-36b82a720cf60ce002a80577e12e4a1a7";
+            content.partitions.root.end = "-1T";
+          };
+        };
       };
     };
     # systeamadeck = {
     #   install.host = "";
-    #   target.sshOptions = ["ProxyJump=${config.dotfiles.domain}"];
+    #   target.sshOptions = sshOptions;
     #   profiles.system.module = {
     #     imports = [inputs.jovian.nixosModules.jovian];
     #     dotfiles.graphical.enable = true;
@@ -105,8 +174,6 @@ in {
     #       user = config.canivete.people.me;
     #     };
     #     networking.networkmanager.enable = true;
-    #     home-manager.sharedModules = nix.toList {home.stateVersion = "24.05";};
-    #     system.stateVersion = "24.05";
 
     #     # jovian.steam.autoStart conflicts
     #     services.displayManager.sddm.enable = false;
--- modules/profiles/nixos.nix
+++ modules/profiles/nixos.nix
@@ -14,47 +14,17 @@ in {
     ...
   }: {
     imports = [inputs.disko.nixosModules.disko];
-    disko.devices.disk.base = {
-      device = mkDefault "/dev/sda";
-      type = "disk";
-      content.type = "gpt";
-      content.partitions = {
-        ESP = {
-          type = "EF00";
-          size = "500M";
-          content.type = "filesystem";
-          content.format = "vfat";
-          content.mountpoint = "/boot";
-        };
-        root = {
-          end = "-1G";
-          content.type = "filesystem";
-          content.format = "ext4";
-          content.mountpoint = "/";
-        };
-        swap = {
-          size = "100%";
-          content = {
-            type = "swap";
-            discardPolicy = "both";
-            resumeDevice = true;
-          };
-        };
-      };
-    };
-    boot = {
-      initrd.availableKernelModules = ["ahci" "usb_storage" "sd_mod"];
-      loader.systemd-boot.enable = true;
-      loader.efi.canTouchEfiVariables = true;
-    };
+    boot.initrd.availableKernelModules = ["ahci" "usb_storage" "sd_mod"];
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
     hardware.enableRedistributableFirmware = mkDefault true;
     hardware.cpu.intel.updateMicrocode = mkDefault config.hardware.enableRedistributableFirmware;
     home-manager.useGlobalPkgs = true;
     home-manager.sharedModules = toList {
       options.dotfiles = options.dotfiles;
       config.dotfiles = config.dotfiles;
+      config.home.stateVersion = "24.05";
     };
-    # TODO extract
     i18n.defaultLocale = "en_US.UTF-8";
     networking.useDHCP = mkDefault true;
     security.sudo.extraRules = toList {
@@ -64,6 +34,7 @@ in {
         options = ["NOPASSWD"];
       };
     };
+    system.stateVersion = "24.05";
     systemd.services = flip mapAttrs' config.home-manager.users (username: _: nameValuePair "home-manager-${username}" {serviceConfig.TimeoutStartSec = mkForce "10m";});
     time.timeZone = "America/Los_Angeles";
     users.mutableUsers = true;
