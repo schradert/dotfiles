cc2723c62e5af187d1be933a0145f18098eb067b
Tristan Schrader <t0rdos@pm.me>
2024-09-25 10:06:04 -0700

    feat: external-secrets
    this allows me to create secrets declaratively that use values from
    dynamically created secrets in other deployments, which is unfortunately common

---
+++ builds/cluster/security/external-secrets.nix
@@ -0,0 +1,58 @@
+{nix, ...}: with nix; let
+  name = "external-secrets-kubernetes";
+  namespace = "security";
+  namespaces = ["home" "kube-system" "office" "media" "storage"];
+  namespaceModules = (forEach namespaces (ns: {
+    clustersecretstores."kubernetes-${ns}" = {
+      spec.provider.kubernetes = {
+        auth.serviceAccount = {inherit name namespace;};
+        remoteNamespace = ns;
+        server.caProvider = {
+          type = "ConfigMap";
+          name = "kube-root-ca.crt";
+          inherit namespace;
+          key = "ca.crt";
+        };
+      };
+    };
+  }));
+  commonModule = {
+    serviceAccounts.${name} = {};
+    clusterRoles.${name}.rules = [
+      {
+        apiGroups = [""];
+        resources = ["secrets"];
+        verbs = ["get" "list" "watch"];
+      }
+      {
+        apiGroups = ["authorization.k8s.io"];
+        resources = ["selfsubjectrulesreviews"];
+        verbs = ["create"];
+      }
+    ];
+    clusterRoleBindings.${name} = {
+      roleRef = {
+        inherit name;
+        kind = "ClusterRole";
+        apiGroup = "rbac.authorization.k8s.io";
+      };
+      subjects = toList {
+        inherit name namespace;
+        kind = "ServiceAccount";
+      };
+    };
+  };
+in {
+  perSystem.dotfiles.helm.external-secrets = {
+    inherit namespace;
+    chart = {
+      repo = "https://charts.external-secrets.io";
+      chart = "external-secrets";
+      version = "0.10.3";
+      sha256 = "sC4APZ5B5zdp2ZF29YZjT2EdaTacR43pnroQqE4TyNQ=";
+    };
+    values.serviceMonitor.enabled = true;
+    # TODO can I use a single ClusterSecretStore?
+    resources = mkMerge (namespaceModules ++ [commonModule]);
+  };
+}
