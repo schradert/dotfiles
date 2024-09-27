ba3a3684185795ea8b9def31dc1c41d38a1671b1
Tristan Schrader <t0rdos@pm.me>
2024-09-27 14:07:49 -0700

    feat: claim /dev/net/tun for VPN in k8s
    this uses generic-device-plugin, which allows us to specify necessary
    /dev resources on nodes that deployments need

---
+++ builds/cluster/kube-system/generic-device-plugin.nix
@@ -0,0 +1,46 @@
+{nix, ...}: let
+  inherit (nix) toList toJSON;
+in {
+  perSystem.dotfiles.helm.generic-device-plugin = {
+    namespace = "kube-system";
+    values.defaultPodOptions.priorityClassName = "system-node-critical";
+    values.controllers.generic-device-plugin = {
+      type = "daemonset";
+      strategy = "RollingUpdate";
+      annotations."reloader.stakater.com/auto" = "true";
+      containers.generic-device-plugin = {
+        image.repository = "ghcr.io/squat/generic-device-plugin";
+        image.tag = "36bfc606bba2064de6ede0ff2764cbb52edff70d@sha256:ba6f0b4cf6c858d6ad29ba4d32e4da11638abbc7d96436bf04f582a97b2b8821";
+        args = ["--log-level" "info" "--config" "/config/config.yaml"];
+        resources.requests.cpu = "10m";
+        resources.limits.memory = "64Mi";
+      };
+    };
+    values.persistence = {
+      config.type = "configMap";
+      config.name = "generic-device-plugin-configmap";
+      config.globalMounts = toList {
+        path = "/config/config.yaml";
+        subPath = "config.yaml";
+        readOnly = true;
+      };
+      dev.type = "hostPath";
+      dev.hostPath = "/dev";
+      dev.globalMounts = [{readOnly = true;}];
+      sys.type = "hostPath";
+      sys.hostPath = "/sys";
+      sys.globalMounts = [{readOnly = true;}];
+      plugins.type = "hostPath";
+      plugins.hostPath = "/var/lib/kubelet/device-plugins";
+    };
+    resources.configMaps.generic-device-plugin-configmap.data."config.yaml" = toJSON {
+      devices = toList {
+        name = "tun";
+        groups = toList {
+          count = 1000;
+          paths = [{path = "/dev/net/tun";}];
+        };
+      };
+    };
+  };
+}
