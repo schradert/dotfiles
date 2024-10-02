422b82009eb0ba0b5b8b0533846cb0070dc4f123
Tristan Schrader <t0rdos@pm.me>
2024-10-02 15:14:29 -0700

    bump: pin cloudflare to 4.43.0
    they are releasing alpha versions now for 5.0.0 which cuases a lot of
    breaking changes and inconveniences

--- builds/cloud/dns.nix
+++ builds/cloud/dns.nix
@@ -3,35 +3,35 @@
   nix,
   ...
 }: let
+  inherit (nix.vals) sops;
   inherit (config.dotfiles) domain;
   inherit (config.canivete.people.my.profiles.default) email;
 in {
   perSystem.canivete.opentofu.workspaces.deploy = {
-    plugins = ["cloudflare/cloudflare"];
+    plugins = ["cloudflare/cloudflare/4.43.0"];
     modules.dns = {
-      provider.cloudflare.api_token = nix.vals.sops "default.yaml#/cloudflare/pat";
-      # TODO prevent this account name hardcoding
+      provider.cloudflare.api_token = sops "default.yaml#/cloudflare/pat";
       data.cloudflare_accounts.main.name = email;
       resource = {
         cloudflare_zone.trdos = {
           account_id = "\${ data.cloudflare_accounts.main.accounts[0].id }";
-          zone = config.dotfiles.domain;
+          zone = domain;
         };
         cloudflare_record.base = {
           name = "@";
-          value = nix.vals.sops "default.yaml#/trdos_ip";
+          content = sops "default.yaml#/trdos_ip";
           type = "A";
           zone_id = "\${ cloudflare_zone.trdos.id }";
         };
         cloudflare_record.www = {
           name = "www";
-          value = "\${ cloudflare_zone.trdos.zone }";
+          content = "\${ cloudflare_zone.trdos.zone }";
           type = "CNAME";
           zone_id = "\${ cloudflare_zone.trdos.id }";
         };
         cloudflare_record.wildcard = {
           name = "*";
-          value = nix.vals.sops "default.yaml#/trdos_ip";
+          content = sops "default.yaml#/trdos_ip";
           type = "A";
           zone_id = "\${ cloudflare_zone.trdos.id }";
         };
