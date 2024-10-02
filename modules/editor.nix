c88dddee6c1f7ebb8d05bcc7d5b4d3586742be22
Tristan Schrader <t0rdos@pm.me>
2024-10-02 11:52:29 -0700

    refactor: consolidate editor module

--- modules/editor.nix
+++
@@ -1,9 +0,0 @@
-{nix, ...}:
-with nix; {
-  canivete.deploy.system.homeModules.editor.options.dotfiles.editor = mkOption {
-    type = enum ["vim" "emacs"];
-    default = "vim";
-    example = "emacs";
-    description = "Default editor to use for profile";
-  };
-}
--- modules/general.nix
+++ modules/general.nix
@@ -7,12 +7,18 @@ in {
     type = strMatching "^[a-z0-9\-]+\.[a-z]{2,}$";
     description = "Base domain for exposing nodes and services";
   };
-  canivete.deploy.system.homeModules.auth = {config, ...}: {
+  canivete.deploy.system.homeModules.general = {config, ...}: {
     options.dotfiles.profile = mkOption {
       type = enum (attrNames users.${config.home.username}.profiles);
       example = "work";
       description = "The dotfiles profile to use for this configuration";
       default = "default";
+    };
+    options.dotfiles.editor = mkOption {
+      type = enum ["vim" "emacs"];
+      default = "vim";
+      example = "emacs";
+      description = "Default editor to use for profile";
     };
   };
 }
