d917a6c2424b28b2076b33de1766c72abc3794a7
Tristan Schrader <t0rdos@pm.me>
2024-10-02 10:49:44 -0700

    refactor: cleaner backblaze opentofu module

--- builds/cloud/backups.nix
+++ builds/cloud/backups.nix
@@ -1,15 +1,15 @@
 {
   perSystem.canivete.opentofu.workspaces.deploy = {
     plugins = ["Backblaze/b2"];
-    modules.backblaze = {nix, ...}: {
-      provider.b2 = {
-        application_key = nix.vals.sops "default.yaml#/backblaze/application_key";
-        application_key_id = nix.vals.sops "default.yaml#/backblaze/application_key_id";
-      };
-      resource.b2_bucket.main = {
-        bucket_name = "t0rdos";
-        bucket_type = "allPrivate";
-      };
+    modules.backblaze = {nix, ...}: let
+      inherit (nix.vals) sops;
+      application_key_id = sops "default.yaml#/backblaze/application_key_id";
+      application_key = sops "default.yaml#/backblaze/application_key";
+      bucket_name = "t0rdos";
+      bucket_type = "allPrivate";
+    in {
+      provider.b2 = {inherit application_key application_key_id;};
+      resource.b2_bucket.main = {inherit bucket_name bucket_type;};
     };
   };
 }
