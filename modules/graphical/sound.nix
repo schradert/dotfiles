306219a5742327d9c5cc213a87f52d6871f7dcfc
Tristan Schrader <t0rdos@pm.me>
2024-11-15 21:43:56 -0800

    feat: add bluetooth and pavucontrol when sound is on

--- modules/graphical/sound.nix
+++ modules/graphical/sound.nix
@@ -1,8 +1,10 @@
 {
   # TODO why do I need to explicitly start the pipewire-pulse service when the socket should activate it?
-  canivete.deploy.nixos.modules.sound = {config, lib, ...}: {
+  canivete.deploy.nixos.modules.sound = {config, lib, pkgs, ...}: {
     options.dotfiles.graphical.sound.enable = lib.mkEnableOption "Sound devices";
     config = lib.mkIf config.dotfiles.graphical.sound.enable {
+      environment.systemPackages = [pkgs.pavucontrol];
+      hardware.bluetooth.enable = true;
       security.rtkit.enable = true;
       services.pipewire = {
         enable = true;
