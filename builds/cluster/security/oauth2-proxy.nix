0f11485c06cec9f37ce1fa6b7873b4f0507f0e22
Tristan Schrader <t0rdos@pm.me>
2024-11-15 21:49:52 -0800

    refactor: [wip] convert failed keycloakConfigCli to OpenTofu

--- builds/cluster/security/oauth2-proxy.nix
+++ builds/cluster/security/oauth2-proxy.nix
@@ -7,17 +7,39 @@
     cookie = "oauth2-proxy-cookie";
     cookiePath = "'[\"oauth2-proxy\"][\"cookie\"]'";
     sopsFile = "\"$(${getExe git} rev-parse --show-toplevel)/.canivete/sops/default.yaml\"";
+    realm_id = "\${ keycloak_realm.primary.id }";
+    client_id = "\${ keycloak_openid_client.oauth2-proxy.client_id }";
   in {
     canivete.opentofu.workspaces.deploy = {
       plugins = ["scottwinkler/shell"];
-      modules.oauth2-proxy.resource.shell_script.${cookie}.lifecycle_commands = {
-        create = ''
-          ${getExe openssl} rand -base64 32 |
-          ${coreutils}/bin/head -c 32 |
-          ${coreutils}/bin/base64 |
-          ${getExe jq} --raw-input '{"value":.}'
-        '';
-        delete = "echo";
+      modules.oauth2-proxy.resource = {
+        shell_script.${cookie}.lifecycle_commands = {
+          create = ''
+            ${getExe openssl} rand -base64 32 |
+            ${coreutils}/bin/head -c 32 |
+            ${coreutils}/bin/base64 |
+            ${getExe jq} --raw-input '{"value":.}'
+          '';
+          delete = "echo";
+        };
+        keycloak_openid_client.oauth2-proxy = {
+          inherit realm_id;
+          client_id = "oauth2-proxy";
+          name = "OAuth2 Proxy";
+          description = "OAuth2 Proxy";
+          access_type = "PUBLIC";
+          standard_flow_enabled = true;
+          valid_redirect_uris = ["https://oauth2-proxy.${domain}/oauth2/callback"];
+        };
+        keycloak_openid_audience_protocol_mapper.oauth2-proxy = {
+          inherit realm_id client_id;
+          name = "aud-mapper-${client_id}";
+          included_client_audience = client_id;
+        };
+        keycloak_openid_client_default_scopes.oauth2-proxy = {
+          inherit realm_id client_id;
+          default_scopes = ["profile" "email" "roles" "web-origins" "\${ keycloak_openid_client_scope.groups.name }"];
+        };
       };
     };
     dotfiles.opentofu.sops.oauth2-proxy-cookie = {
