2898e14a21f8b9b5bd19d83eb1229012e51cca11
Tristan Schrader <t0rdos@pm.me>
2025-02-07 00:24:16 -0800

    feat: unison language

---
+++ modules/shell/unison.nix
@@ -0,0 +1,9 @@
+{inputs, ...}: {
+  flake.overlays.unison = inputs.unison.overlays.default;
+  canivete.deploy.system.homeModules.unison = {pkgs, ...}: {
+    # TODO where to include tree-sitter plugin tree-sitter-unison?
+    # TODO rss: https://www.unison-lang.org/blog/
+    # programs.emacs.extraPackages = epkgs: [epkgs.unison-ts-mode];
+    programs.vim.plugins = with pkgs.vimPlugins; [vim-unison];
+  };
+}
