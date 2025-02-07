30fad05d6e4d6c84c23f04bb66fb579dec44bd77
Tristan Schrader <t0rdos@pm.me>
2025-02-07 00:32:17 -0800

    feat: kdeconnect and qtscrcpy

---
+++ modules/graphical/connect.nix
@@ -0,0 +1,18 @@
+{
+  canivete.deploy = {
+    # Opens firewall
+    nixos.modules.connect = {config, ...}: {programs.kdeconnect.enable = config.dotfiles.graphical.enable;};
+    system.homeModules.connect = {
+      config,
+      lib,
+      pkgs,
+      ...
+    }: {
+      config = lib.mkIf (config.dotfiles.graphical.enable && pkgs.stdenv.hostPlatform.isLinux) {
+        home.packages = with pkgs; [kdePackages.kdeconnect-kde qtscrcpy];
+        services.kdeconnect.enable = true;
+        services.kdeconnect.indicator = true;
+      };
+    };
+  };
+}
