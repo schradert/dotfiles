9f453f2ea3376d8258053988c5ab2d12f1eea212
Tristan Schrader <t0rdos@pm.me>
2025-02-07 00:25:19 -0800

    feat: bat config

---
+++ modules/shell/bat.nix
@@ -0,0 +1,12 @@
+{
+  canivete.deploy.system.homeModules.bat = {pkgs, ...}: {
+    programs.bat = {
+      enable = true;
+      config.theme = "Dracula";
+      # TODO why are these broken? file doesn't say anything about that
+      # extraPackages = with pkgs.bat-extras; [prettybat batwatch batpipe batman batgrep batdiff];
+      # TODO themes (how can it automatically choose by the wallpaper or my theme switcher?)
+      # TODO syntaxes
+    };
+  };
+}
