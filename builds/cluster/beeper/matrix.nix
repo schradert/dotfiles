04966c6ba2bc7c0a3f47f80824fd38178c7605e6
Tristan Schrader <t0rdos@pm.me>
2025-02-07 00:31:37 -0800

    feat: iamb

---
+++ builds/cluster/beeper/matrix.nix
@@ -0,0 +1,8 @@
+{
+  # TODO self-hosted matrix server (dendrite, synapse, etc.)
+  canivete.deploy.system.homeModules.matrix = {config, lib, pkgs, ...}: {
+    config = lib.mkIf config.dotfiles.profiles.chat.enable {
+      home.packages = [pkgs.iamb];
+    };
+  };
+}
