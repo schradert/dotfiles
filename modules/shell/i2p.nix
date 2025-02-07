885620f10429f5703b866fe3d109b82d19849280
Tristan Schrader <t0rdos@pm.me>
2025-02-07 00:25:28 -0800

    feat: i2p

---
+++ modules/shell/i2p.nix
@@ -0,0 +1,12 @@
+{
+  # TODO which one should I use?
+  # TODO configure qbittorrent in mixed mode
+  # TODO activate SAM protocol and follow i2pd docs
+  # TODO how to get I2P+ (prettiest frontend) https://i2pplus.github.io/
+  canivete.deploy.system.homeModules.i2p = {config, lib, pkgs, ...}: {
+    options.dotfiles.programs.i2p.enable = lib.mkEnableOption "I2P router";
+    config = lib.mkIf config.dotfiles.programs.i2p.enable {
+      home.packages = [pkgs.i2pd];
+    };
+  };
+}
