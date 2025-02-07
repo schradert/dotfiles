0404c99bb0f1dab2383e365591020edc6a4e3513
Tristan Schrader <t0rdos@pm.me>
2025-02-07 00:39:37 -0800

    feat: more kube tools

--- modules/shell/k9s.nix
+++ modules/shell/k9s.nix
@@ -3,7 +3,7 @@
   # TODO build https://github.com/hcavarsan/kftray
   # TODO https://github.com/derailed/k9s/blob/master/plugins/carvel.yaml
   canivete.deploy.system.homeModules.k9s = {pkgs, ...}: {
-    home.packages = with pkgs; [kubectl kubernetes-helm];
+    home.packages = with pkgs; [kubectl kubernetes-helm kubetui kdash ktop];
     programs.k9s.enable = true;
     # Reference "https://github.com/derailed/k9s/blob/master/skins/dracula.yaml"
     programs.k9s.skins.skin.k9s = let
