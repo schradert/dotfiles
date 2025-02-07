98a866a70f0b15b87b61f1a4dee9b9774cc7e9f7
Tristan Schrader <t0rdos@pm.me>
2025-02-06 19:30:49 -0800

    feat: option for irc channels

---
+++ builds/cluster/home/irc.nix
@@ -0,0 +1,21 @@
+{lib, ...}: {
+  # NOTE CHANNELS
+  # TODO https://github.com/ergochat/ergo
+  # TODO https://codeberg.org/emersion/soju
+  # TODO https://github.com/progval/Limnoria
+  # TODO https://github.com/oddluck/limnoria-plugins
+  # TODO https://github.com/Libera-Chat/ozone
+  # TODO https://codeberg.org/emersion/gamja
+  # TODO https://sr.ht/~delthas/senpai/
+  # TODO https://www.mirc.com/ vs irssi vs weechat
+  # #invidious:libera.chat
+  # #django:libera.chat
+  # #spritely:libera.chat
+  # #indieweb:libera.chat
+  # #protondb:libera.chat
+  # #octoprint:libera.chat
+  options.dotfiles.irc.channels = lib.mkOption {
+    type = with lib.types; listOf str;
+    default = [];
+  };
+}
