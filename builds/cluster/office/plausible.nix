145f2099735a8e142bf2b67d6a1c05d6c3c66c97
Tristan Schrader <t0rdos@pm.me>
2025-02-07 00:31:45 -0800

    feat: conclusive (plausible tui)

---
+++ builds/cluster/office/plausible.nix
@@ -0,0 +1,11 @@
+{
+  # TODO https://github.com/plausible/analytics
+  # TODO or alternative https://github.com/matomo-org/matomo
+  canivete.deploy.system.homeModules.conclusive = {config, lib, pkgs, ...}: {
+    options.dotfiles.programs.conclusive.enable = lib.mkEnableOption "conclusive";
+    config = lib.mkIf config.dotfiles.programs.conclusive.enable {
+      # TODO build https://github.com/mrusme/conclusive
+      # home.packages = [pkgs.conclusive];
+    };
+  };
+}
