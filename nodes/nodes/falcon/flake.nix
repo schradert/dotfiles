3edaa4392c0bd3428066a460659f9d591d34345c
Tristan Schrader <t0rdos@pm.me>
2025-06-07 19:22:19 -0700

    fix: avoid ~/.gtkrc-2.0 clash in plasma

--- nodes/nodes/falcon/flake.nix
+++ nodes/nodes/falcon/flake.nix
@@ -127,8 +127,15 @@
           services.displayManager.sddm.settings.General.DisplayServer = "wayland";
           services.xserver.enable = true;
 
-          # TODO why isn't the builtin Plasma 6 notification daemon not working?
-          home-manager.sharedModules = [{services.swaync.enable = true;}];
+          home-manager.sharedModules = [
+            {
+              # Seems like this file always conflicts in a Plasma session...
+              # NOTE https://github.com/nix-community/home-manager/issues/6188#issuecomment-2749859294
+              gtk.gtk2.force = true;
+              # TODO why isn't the builtin Plasma 6 notification daemon not working?
+              services.swaync.enable = true;
+            }
+          ];
         }
         ({pkgs, ...}: {
           # Gaming
