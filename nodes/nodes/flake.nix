e74d3ca1b0af5634f5c2d8c40399f15567d338a6
Tristan Schrader <t0rdos@pm.me>
2025-06-10 18:58:23 -0700

    feat: nix-index + comma + database

--- nodes/nodes/flake.nix
+++ nodes/nodes/flake.nix
@@ -8,9 +8,19 @@
     nixos-facter-modules.url = "github:nix-community/nixos-facter-modules";
     nix-alien.url = "github:thiagokokada/nix-alien";
     nix-alien.inputs.nixpkgs.follows = "nixpkgs";
+    nix-alien.inputs.flake-compat.follows = "flake-compat";
+    nix-alien.inputs.nix-index-database.follows = "nix-index-database";
     stylix.url = "github:nix-community/stylix";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
+    stylix.inputs.flake-compat.follows = "flake-compat";
     stylix.inputs.home-manager.follows = "home-manager";
+    nix-index.url = "github:nix-community/nix-index";
+    nix-index.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index.inputs.flake-compat.follows = "flake-compat";
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-compat.url = "github:edolstra/flake-compat";
   };
   outputs = inputs: {
     lib = let
@@ -545,9 +555,31 @@
       };
       nix = {
         home-manager.sharedModules = [
-          ({pkgs, ...}: {
+          inputs.nix-index-database.hmModules.nix-index
+          ({config, pkgs, ...}: {
             home.packages = with pkgs; [nix-inspect nix-fast-build];
             nix.extraOptions = "experimental-features = nix-command flakes";
+            programs.nix-index.package = pkgs.nix-index-with-db;
+            programs.nix-index-database.comma.enable = true;
+            programs.nushell.extraConfig = "$env.config.hooks.command_not_found = source ${config.programs.nix-index.package}/etc/profile.d/command-not-found.nu";
+            # TODO elvish + xonsh adapters for command-not-found hook
+          })
+        ];
+        # Nushell script isn't packaged yet or updated to removed --top-level
+        nixpkgs.overlays = [
+          inputs.nix-index-database.overlays.nix-index
+          (final: prev: {
+            nix-index-unwrapped = inputs.nix-index.packages.${prev.system}.default;
+            nix-index-with-db = prev.nix-index-with-db.overrideAttrs (old: {
+              buildCommand = old.buildCommand + ''
+                rm -f "$out/etc/profile.d/command-not-found.nu"
+                substitute \
+                  "${final.nix-index-unwrapped}/etc/profile.d/command-not-found.nu" \
+                  "$out/etc/profile.d/command-not-found.nu" \
+                  --replace-fail "${final.nix-index-unwrapped}" "$out"
+                sed --in-place '32s/--top-level //' "$out/etc/profile.d/command-not-found.nu"
+              '';
+            });
           })
         ];
       };
