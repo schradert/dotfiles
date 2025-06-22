6688afe9cd558a9d9d62d3bcb4c5415451622e8e
Tristan Schrader <t0rdos@pm.me>
2025-06-21 21:06:48 -0700

    feat: snapshot-controller + openebs

--- infra/default.nix
+++ infra/default.nix
@@ -87,6 +87,8 @@
     #   cert-manager.provider.cloudflare.token = canivete.vals.sops.default "cloudflare/token";
       cilium.enable = true;
       coredns.enable = true;
+      snapshot-controller.enable = true;
+      openebs.enable = true;
     #   descheduler.enable = true;
     #   external-dns.enable = true;
     #   external-secrets.enable = true;
@@ -94,10 +96,8 @@
     #   kubelet-csr-approver.enable = true;
     #   oauth2-proxy.enable = true;
     #   nginx.enable = true;
-    #   openebs.enable = true;
     #   prometheus.enable = true;
     #   reloader.enable = true;
-    #   snapshot-controller.enable = true;
     #   spegel.enable = true;
     #   volsync.enable = true;
 
