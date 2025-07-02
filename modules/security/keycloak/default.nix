08b80251a994181e61a705ae5916a540e32a2959
Tristan Schrader <t0rdos@pm.me>
2025-07-02 11:57:16 -0700

    feat: deploy keycloak

--- modules/security/keycloak/default.nix
+++ modules/security/keycloak/default.nix
@@ -7,33 +7,33 @@
   }: let
     inherit (config) domain;
     inherit (config.services) keycloak external-secrets postgres;
-    inherit (lib) mkEnableOption mkIf mkMerge;
-    subdomain = "keycloak.${domain}";
+    inherit (lib) mkEnableOption mkIf mkMerge toList;
+    hostname = "keycloak.${domain}";
     image = {
-      imageName = "";
-      imageDigest = "";
-      hash = "";
-      finalImageTag = "";
+      imageName = "bitnami/keycloak";
+      imageDigest = "sha256:17aff522766636a7a315188481662b4bf4ce846731c06da261ba5b6a95fea70d";
+      hash = "sha256-ZGLUzdTmiCkEP5yYVbpAO8lxzqbG0lIHkknKy3QI3bc=";
+      finalImageTag = "26.2.5-debian-12-r3";
     };
   in {
     options.services.keycloak.enable = mkEnableOption "keycloak";
     config = mkIf keycloak.enable {
       nixos = {pkgs, ...}: {canivete.kubernetes.images.keycloak = pkgs.dockerTools.pullImage image;};
+      opentofu.passwords.keycloak-superadmin.length = 21;
       kubenix = {helm, ...}: {
         dotfiles.postgres.keycloak = {};
-        opentofu.passwords.keycloak-superadmin.length = 21;
         kubernetes.helm.releases.keycloak = {
           namespace = "security";
           chart = helm.fetch {
             chartUrl = "oci://registry-1.docker.io/bitnamicharts/keycloak";
             chart = "keycloak";
-            version = "22.2.5";
-            sha256 = "t1bM5+uhcWmbrFQ5HfCTcxbCjiK624kIaaPrv6Q12ok=";
+            version = "24.7.4";
+            sha256 = "sha256-HZIRUzTfgTktIo3okfqGmnJ71gezFunGlbRkgsc73BE=";
           };
           values = {
             auth.adminUser = "superadmin";
             auth.existingSecret = "keycloak";
-            auth.passwordSecretKey = "keycloak-superadmin";
+            auth.passwordSecretKey = "superadmin";
             adminRealm = "admin";
             production = true;
             proxyHeaders = "xforwarded";
@@ -42,12 +42,6 @@
             livnessProbe.initialDelaySeconds = 0;
             readinessProbe.initialDelaySeconds = 0;
             podAnnotations."reloader.stakater.com/auto" = "true";
-            ingress = {
-              enabled = true;
-              ingressClassName = "external";
-              annotations."external-dns.alpha.kubernetes.io/target" = "external.${domain}";
-              hostname = subdomain;
-            };
             rbac.create = true;
             autoscaling.enabled = true;
             autoscaling.maxReplicas = 2;
@@ -71,10 +65,24 @@
               configMaps.keycloak.data = {
                 KC_DB = "postgres";
                 KC_FEATURES = "hostname:v2";
-                KC_HOSTNAME = subdomain;
+                KC_HOSTNAME = hostname;
                 KC_METRICS_ENABLED = "true";
                 KC_HEALTH_ENABLED = "true";
                 KC_HTTP_ENABLED = "true";
+              };
+              httproutes.keycloak.spec = {
+                hostnames = [hostname];
+                parentRefs = toList {
+                  name = "external";
+                  namespace = "kube-system";
+                  sectionName = "https";
+                };
+                rules = toList {
+                  backendRefs = toList {
+                    name = "{{ template \"common.names.fullname\" . }}";
+                    port = "http";
+                  };
+                };
               };
             }
             (mkIf (external-secrets.enable && postgres.enable) {
@@ -84,7 +92,7 @@
                 dataFrom = [{extract.key = "keycloak.main.credentials.postgresql.acid.zalan.do";}];
                 target.template.data = {
                   db-password = "{{ .password }}";
-                  keycloak-superadmin = canivete.vals.sops.default "passwords/keycloak-superadmin";
+                  superadmin = canivete.vals.sops.default "passwords/keycloak-superadmin";
                 };
               };
             })
---
+++ modules/security/keycloak/opentofu.nix
@@ -0,0 +1,108 @@
+{
+  # NOTE https://www.keycloak.org/2024/09/realm-config-management-tools-survey-results
+  dotfiles = {config, lib, ...}: let
+    realm_id = "\${ keycloak_realm.primary.id }";
+    hostname = "keycloak.${config.domain}";
+  in {
+    options.services.keycloak.opentofu.enable = lib.mkEnableOption "OpenTofu configuration of Keycloak";
+    config = lib.mkIf config.services.keycloak.opentofu.enable {
+      opentofu.passwords.keycloak-tristan.length = 21;
+      opentofu.plugins = ["mrparkers/keycloak"];
+      opentofu.modules = {
+        provider.keycloak = {
+          client_id = "admin-cli";
+          username = "superadmin";
+          password = "\${ random_password.keycloak-superadmin.result }";
+          url = "https" + "://${hostname}";
+        };
+        resource = {
+          keycloak_realm.primary = {
+            realm = "primary";
+            enabled = true;
+            display_name = "Primary (${hostname})";
+            registration_allowed = false;
+          };
+          keycloak_user.tristan = {
+            inherit realm_id;
+            username = "tristan";
+            email = "tristanschrader@pm.me";
+            first_name = "Tristan";
+            last_name = "Schrader";
+            # TODO initial password
+          };
+          keycloak_group.admin = {
+            inherit realm_id;
+            name = "admin";
+          };
+          keycloak_group.family = {
+            inherit realm_id;
+            name = "family";
+          };
+          keycloak_group_memberships.admin = {
+            inherit realm_id;
+            members = ["tristan"];
+            group_id = "\${ keycloak_group.admin.id }";
+          };
+          keycloak_group_memberships.family = {
+            inherit realm_id;
+            members = ["tristan"];
+            group_id = "\${ keycloak_group.family.id }";
+          };
+          keycloak_openid_client_scope.groups = {
+            inherit realm_id;
+            name = "groups";
+            description = "When requested, this scope will map a user's group memberships to a claim";
+            include_in_token_scope = true;
+          };
+          keycloak_openid_group_membership_protocol_mapper.groups = {
+            realm_id = "\${ keycloak_realm.primary.id }";
+            client_scope_id = "\${ keycloak_openid_client_scope.groups.id }";
+            name = "group-membership-mapper";
+            claim_name = "groups";
+          };
+          keycloak_required_action.fido2 = {
+            inherit realm_id;
+            alias = "webauthn-register-passwordless";
+            enabled = true;
+            name = "WebAuthn Register Passwordless";
+            default_action = true;
+          };
+          keycloak_authentication_bindings.browser = {
+            inherit realm_id;
+            browser_flow = "\${ keycloak_authentication_flow.fido2.alias }";
+          };
+          keycloak_authentication_flow.fido2 = {
+            inherit realm_id;
+            alias = "fido2";
+          };
+          keycloak_authentication_execution.cookie = {
+            inherit realm_id;
+            parent_flow_alias = "\${ keycloak_authentication_flow.fido2.alias }";
+            authenticator = "auth-cookie";
+            requirement = "ALTERNATIVE";
+          };
+          keycloak_authentication_subflow.fido2 = {
+            inherit realm_id;
+            alias = "fido2-subflow";
+            parent_flow_alias = "\${ keycloak_authentication_flow.fido2.alias }";
+            requirement = "REQUIRED";
+            depends_on = ["keycloak_authentication_execution.cookie"];
+          };
+          keycloak_authentication_execution.username = {
+            inherit realm_id;
+            parent_flow_alias = "\${ keycloak_authentication_subflow.fido2.alias }";
+            authenticator = "username-form";
+            requirement = "REQUIRED";
+          };
+          keycloak_authentication_execution.fido2 = {
+            inherit realm_id;
+            parent_flow_alias = "\${ keycloak_authentication_subflow.fido2.alias }";
+            authenticator = "webauthn-passwordless";
+            requirement = "REQUIRED";
+            depends_on = ["keycloak_authentication_execution.username"];
+          };
+        };
+      };
+    };
+  };
+}
