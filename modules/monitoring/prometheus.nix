6e5fa0a9e745c1823b3ef3309b6b44f1c544111e
Tristan Schrader <t0rdos@pm.me>
2025-07-21 16:52:03 -0700

    chore(prometheus): bump chart + images

--- modules/monitoring/prometheus.nix
+++ modules/monitoring/prometheus.nix
@@ -4,49 +4,54 @@
     lib,
     ...
   }: let
+    inherit (builtins) concatStringsSep head mapAttrs tail;
     inherit (config) domain;
-    inherit (lib) getExe mkEnableOption mkIf toList;
-    certgen-tag = "v1.5.2";
-    admission-tag = "v0.83.0";
+    inherit (lib) getExe mkEnableOption mkIf splitString toList;
+    pinImage = image: let
+      parts = splitString "/" image.imageName;
+    in {
+      registry = head parts;
+      repository = concatStringsSep "/" (tail parts);
+      tag = image.finalImageTag;
+      pullPolicy = "Never";
+    };
+    appVersion = "v0.84.0";
+    images = {
+      prometheus = {
+        imageName = "quay.io/prometheus/prometheus";
+        imageDigest = "sha256:63805ebb8d2b3920190daf1cb14a60871b16fd38bed42b857a3182bc621f4996";
+        hash = "sha256-AN+3pMNugZVfmqbU0typ+GnNDs/+eVSOZUg+Gh1Cuf8=";
+        finalImageTag = "v3.5.0";
+      };
+      prometheus-operator = {
+        imageName = "quay.io/prometheus-operator/prometheus-operator";
+        imageDigest = "sha256:f128a6969e458a58cc51befebe98bf78a18060be0f7bf62ca91a271505eb7875";
+        hash = "sha256-USePLknFULGGZ93XPSr7fBmsTbSEKPLSFkyC4gYtv3k=";
+        finalImageTag = appVersion;
+      };
+      prometheus-admission-webhook = {
+        imageName = "quay.io/prometheus-operator/admission-webhook";
+        imageDigest = "sha256:72a1f42d7439864b171d91a1d6f0202bdd48913bff1d0f11e5cab47d04c74351";
+        hash = "sha256-LsUUecnDDa49xGaZCRus8D6ghuumxV9JfNfTw/CmdRo=";
+        finalImageTag = appVersion;
+      };
+      prometheus-config-reloader = {
+        imageName = "quay.io/prometheus-operator/prometheus-config-reloader";
+        imageDigest = "sha256:697a5fdc409d8fbec55cfa389b62bbde04e74aaa6b67485a0ab8bd4ee5ab4b5c";
+        hash = "sha256-0Vh+FhYfELEsGQlpLomljMcxsCsN6+c9QiKyHPEbOIw=";
+        finalImageTag = appVersion;
+      };
+      prometheus-certgen = {
+        imageName = "registry.k8s.io/ingress-nginx/kube-webhook-certgen";
+        imageDigest = "sha256:c9f76a75fd00e975416ea1b73300efd413116de0de8570346ed90766c5b5cefb";
+        hash = "sha256-t/DVrYCqwzixjtXkXFOtLCpRk4dHF43knpwCANz6wxc=";
+        finalImageTag = "v1.6.0";
+      };
+    };
   in {
     options.services.prometheus.enable = mkEnableOption "prometheus";
     config = mkIf config.services.prometheus.enable {
-      nixos = {pkgs, ...}: let
-        inherit (pkgs.dockerTools) pullImage;
-      in {
-        canivete.kubernetes.images = {
-          prometheus-operator = pullImage {
-            imageName = "quay.io/prometheus-operator/prometheus-operator";
-            imageDigest = "sha256:5f6a204b252e901b97486ff409c74f48cdbb4cf83731355b08f1155febad6822";
-            hash = "sha256-Eb2xUZuXmti3LClShjMHnht48LeftkH/NJFQN0qfT0Q=";
-            finalImageTag = "v0.81.0";
-          };
-          prometheus = pullImage {
-            imageName = "quay.io/prometheus/prometheus";
-            imageDigest = "sha256:6927e0919a144aa7616fd0137d4816816d42f6b816de3af269ab065250859a62";
-            hash = "sha256-k0C4Y1xNBPy+OA4OKINiyE+4gcHQf8YelJ8p7nwH/XI=";
-            finalImageTag = "v3.2.1";
-          };
-          prometheus-admission-webhook = pullImage {
-            imageName = "quay.io/prometheus-operator/admission-webhook";
-            imageDigest = "sha256:b0d9c9e531e9d40e91d0789de1e4839d9218c18c5071b229408265caad3f39f5";
-            hash = "sha256-7Xo/AsDDAfmwj89bPwvmDZqg+enBBvpkgd1OXXH205o=";
-            finalImageTag = admission-tag;
-          };
-          prometheus-config-reloader = pullImage {
-            imageName = "quay.io/prometheus-operator/prometheus-config-reloader";
-            imageDigest = "sha256:959d47672fbff2776a04ec62b8afcec89e8c036af84dc5fade50019dab212746";
-            hash = "sha256-M6icjqAoMG3v7GjO4pakn1QIj0gwA2m8CROIKJxzSG4=";
-            finalImageTag = "v0.81.0";
-          };
-          prometheus-certgen = pullImage {
-            imageName = "registry.k8s.io/ingress-nginx/kube-webhook-certgen";
-            imageDigest = "sha256:e8825994b7a2c7497375a9b945f386506ca6a3eda80b89b74ef2db743f66a5ea";
-            hash = "sha256-JS2+CtDDTOD8TcXtfTdCUD9Bo6MfGAYkWfO7o/+uo+E=";
-            finalImageTag = certgen-tag;
-          };
-        };
-      };
+      nixos = {pkgs, ...}: {canivete.kubernetes.images = mapAttrs (_: pkgs.dockerTools.pullImage) images;};
       nixidy = {
         charts,
         pkgs,
@@ -76,23 +81,28 @@
               crds.enabled = false;
               kubelet.enabled = true;
               kubeApiServer.enabled = true;
-              prometheus.prometheusSpec.storageSpec.volumeClaimTemplate.spec = {
-                accessModes = ["ReadWriteOnce"];
-                resources.requests.storage = "10Gi";
-              };
-              prometheus.route.main = {
-                enabled = true;
-                hostnames = ["prometheus.${domain}"];
-                parentRefs = toList {
-                  name = "internal";
-                  namespace = "kube-system";
-                  sectionName = "https";
+              prometheus = {
+                prometheusSpec.image = pinImage images.prometheus;
+                prometheusSpec.storageSpec.volumeClaimTemplate.spec = {
+                  accessModes = ["ReadWriteOnce"];
+                  resources.requests.storage = "10Gi";
+                };
+                route.main = {
+                  enabled = true;
+                  hostnames = ["prometheus.${domain}"];
+                  parentRefs = toList {
+                    name = "internal";
+                    namespace = "kube-system";
+                    sectionName = "https";
+                  };
                 };
               };
-              prometheusOperator.admissionWebhooks = {
-                deployment.enabled = true;
-                deployment.image.tag = admission-tag;
-                patch.image.tag = certgen-tag;
+              prometheusOperator = {
+                admissionWebhooks.deployment.enabled = true;
+                admissionWebhooks.deployment.image = pinImage images.prometheus-admission-webhook;
+                admissionWebhooks.patch.image = pinImage images.prometheus-certgen;
+                image = pinImage images.prometheus-operator;
+                prometheusConfigReloader.image = pinImage images.prometheus-config-reloader;
               };
 
               # Deployed separately
