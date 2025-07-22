c947d664cfef661af9ab318193a6df654a67bf6a
Tristan Schrader <t0rdos@pm.me>
2025-07-21 17:02:17 -0700

    chore: bump loki

--- modules/monitoring/loki.nix
+++ modules/monitoring/loki.nix
@@ -6,9 +6,9 @@
   }: let
     image = {
       imageName = "grafana/loki";
-      imageDigest = "sha256:a74594532eec4cc313401beedc4dd2708c43674c032084b1aeb87c14a5be1745";
-      hash = "sha256-tgP1TCoykKpKCTyn/S4FLkd+dYAYYJlKX0DZTy+Y1RU=";
-      finalImageTag = "3.5.1";
+      imageDigest = "sha256:d12ab5b1cd7ac58c1c0ea8cb43e0d151d1d5bee1cb897c7e7eaf225adae07928";
+      hash = "sha256-UNmBIlcCwaJ9fK5U9GuLFqfdlZgiX/U6eoM87UkdwB8=";
+      finalImageTag = "3.5.2";
     };
   in {
     options.services.loki.enable = lib.mkEnableOption "loki";
@@ -23,27 +23,32 @@
               deploymentMode = "SingleBinary";
               backend.replicas = 0;
               gateway.replicas = 0;
-              loki.commonConfig.replication_factor = 1;
               read.replicas = 0;
               singleBinary.replicas = 1;
+              singleBinary.persistence.enabled = true;
               write.replicas = 0;
 
-              singleBinary.persistence.enabled = true;
-              loki.storage.type = "filesystem";
-              loki.compactor = {
-                working_directory = "/var/loki/compactor/retention";
-                delete_request_store = "filesystem";
-                retention_enabled = true;
-              };
+              loki = {
+                commonConfig.replication_factor = 1;
+                storage.type = "filesystem";
+                image.repository = image.imageName;
+                image.tag = image.finalImageTag;
+                image.pullPolicy = "Never";
+                compactor = {
+                  working_directory = "/var/loki/compactor/retention";
+                  delete_request_store = "filesystem";
+                  retention_enabled = true;
+                };
 
-              # Seems like I HAVE to define this: https://grafana.com/docs/loki/latest/operations/storage/schema/
-              loki.schemaConfig.configs = lib.toList {
-                from = "2024-04-01";
-                object_store = "filesystem";
-                store = "tsdb";
-                schema = "v13";
-                index.prefix = "index_";
-                index.period = "24h";
+                # Seems like I HAVE to define this: https://grafana.com/docs/loki/latest/operations/storage/schema/
+                schemaConfig.configs = lib.toList {
+                  from = "2024-04-01";
+                  object_store = "filesystem";
+                  store = "tsdb";
+                  schema = "v13";
+                  index.prefix = "index_";
+                  index.period = "24h";
+                };
               };
             };
           };
