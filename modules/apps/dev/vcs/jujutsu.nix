fbf64a4002038594be3666dd85c45ac4688e6fec
Tristan Schrader <t0rdos@pm.me>
2025-08-04 11:44:47 -0700

    feat: jujutsu accessories/completions

--- modules/apps/dev/vcs/jujutsu.nix
+++ modules/apps/dev/vcs/jujutsu.nix
@@ -1,4 +1,11 @@
 {
+  dotfiles.nixos = {
+    config,
+    lib,
+    ...
+  }: {
+    dotfiles.nixpkgs.config.allowUnfreePackages = lib.mkIf config.dotfiles.profiles.client.workstation.enable ["vscode-extension-visualjj-visualjj"];
+  };
   dotfiles.home-manager = {
     config,
     lib,
@@ -6,23 +13,35 @@
     ...
   }: let
     inherit (config.programs.git) extraConfig userEmail userName;
+    jj = lib.getExe config.programs.jujutsu.package;
   in {
     config = lib.mkIf config.dotfiles.profiles.client.workstation.enable {
-      home.packages = with pkgs; [gg-jj jjui lazyjj watchman];
-      programs.jujutsu.enable = true;
-      programs.jujutsu.settings = {
-        core.fsmonitor = "watchman";
-        signing = {
-          backend = "ssh";
-          backends.ssh.allowed-signers = extraConfig.gpg.ssh.allowedSignersFile;
-          backends.ssh.program = extraConfig.gpg.ssh.program;
-          key = extraConfig.user.signingKey;
-          sign-all = true;
+      home.packages = with pkgs; [gg-jj jjui jj-fzf lazyjj watchman];
+      dotfiles.programs.nushell.sources.jj = "${jj} util completion nushell";
+      dotfiles.programs.xonsh.interactiveExtra = "source-bash $(${jj} util completion)";
+      programs = {
+        bash.initExtra = "source <(COMPLETE=bash ${jj})";
+        zsh.initContent = "source <(COMPLETE=zsh ${jj})";
+
+        doom-emacs.extraPackages = e: [e.vc-jj e.jjdescription];
+        vim.plugins = [pkgs.vimPlugins.vim-jjdescription];
+        vscode.profiles.default.extensions = [pkgs.vscode-extensions.visualjj.visualjj];
+        jujutsu.enable = true;
+        jujutsu.settings = {
+          core.fsmonitor = "watchman";
+          signing = {
+            backend = "ssh";
+            backends.ssh.allowed-signers = extraConfig.gpg.ssh.allowedSignersFile;
+            backends.ssh.program = extraConfig.gpg.ssh.program;
+            behavior = "own";
+            key = extraConfig.user.signingKey;
+          };
+          ui.conflict-marker-style = "git";
+          ui.diff-formatter = ":git";
+          ui.pager = "delta";
+          user.email = userEmail;
+          user.name = userName;
         };
-        ui.diff.format = "git";
-        ui.pager = "delta";
-        user.email = userEmail;
-        user.name = userName;
       };
     };
   };
